6a280a2b1465c478403637cc9c7263ada5e6c60b
Joe Huang <b93902098@ntu.edu.tw>
2015-07-26 07:50:36 +0800

    Create 168.py

---
+++ 168.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return a string
+    def convertToTitle(self, num):
+        result = ""
+        num -= 1
+        while True:
+            if num < 0:
+                break
+            current_digit = num % 26
+            num = num / 26 - 1
+            current_alphabet = chr(ord('A') + current_digit)
+            result = "{0}{1}".format(result, current_alphabet)
+        result = result[::-1]
+        return result
