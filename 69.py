257934b84d0811a0a10dcfafaaddd1fda9572011
Joe Huang <b93902098@ntu.edu.tw>
2015-10-21 10:17:54 -0400

    Create 69.py

---
+++ 69.py
@@ -0,0 +1,18 @@
+import sys
+
+# Newton's method, Babylonian method
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        r = x
+        while r * r > x:
+            r = (r + x / r) / 2
+        return r
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.mySqrt(int(sys.argv[1]))
