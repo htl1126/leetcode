55b192a7d08db171752605c9fb9b9fb3278245b0
Joe Huang <b93902098@ntu.edu.tw>
2015-10-21 10:13:56 -0400

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,21 @@
+import sys 
+
+# Extended prob: what if there are multiple targets?
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """ 
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lookup_dict = {}
+        for i in xrange(len(nums)):
+            if nums[i] in lookup_dict:
+                return [lookup_dict[nums[i]], i + 1]
+            else:
+                lookup_dict[target - nums[i]] = i + 1 
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.twoSum([2, 7, 11, 15], 9)
