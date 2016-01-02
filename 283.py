8a0fc5533edc6a5063812404b9517ccb8f1c0c04
Joe Huang <b93902098@ntu.edu.tw>
2016-01-01 22:05:19 -0500

    Create 283.py

---
+++ 283.py
@@ -0,0 +1,23 @@
+# make use of 'del' in Python, inspired by:
+# https://leetcode.com/discuss/75850/my-simple-and-clear-python-solution-64ms
+
+import sys
+from leetcode_util import read_list
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        for _ in xrange(len(nums)):
+            if nums[i] == 0:
+                del nums[i]
+                nums.append(0)
+            else:
+                i += 1
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.moveZeroes(read_list(sys.argv[1]))
