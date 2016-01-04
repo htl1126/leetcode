e33b4cee3b15de9eea9ea2d1a074335f58128a83
Joe Huang <b93902098@ntu.edu.tw>
2016-01-04 12:47:58 -0500

    Create 237.py

---
+++ 237.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
+if __name__ == '__main__':
+    sol = Solution()
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    sol.deleteNode(head.next)
+    while head:
+        print head.val
+        head = head.next
