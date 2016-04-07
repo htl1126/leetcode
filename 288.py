d40272579ec199cd050daf3dfb03b9222e897fa6
Joe Huang <b93902098@ntu.edu.tw>
2016-04-07 01:00:29 -0400

    Update 288.py

--- 288.py
+++ 288.py
@@ -4,20 +4,14 @@ class ValidWordAbbr(object):
         initialize your data structure here.
         :type dictionary: List[str]
         """
-        self.lookup = {}
+        self.lookup = collections.defaultdict(set)
         for word in dictionary:
             if len(word) > 2:
                 new_word = '{0}{1}{2}'.format(word[0], len(word) - 2, word[-1])
             else:
                 new_word = word
-            if new_word in self.lookup:
-                self.lookup[new_word].append(word)
-            else:
-                self.lookup[new_word] = [word]
-        for word in self.lookup:
-            self.lookup[word] = set(self.lookup[word])
+            self.lookup[new_word].add(word)
 
-                
     def isUnique(self, word):
         """
         check if a word is unique.
