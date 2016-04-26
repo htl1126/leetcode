f4b4836aa52c4d730cda6567ef276fa64df31c63
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-04-26 19:17:06 -0400

    Create 146.py

---
+++ 146.py
@@ -0,0 +1,40 @@
+import collections
+
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
+
+    def get(self, key):
+        """
+        :rtype: int
+        """
+        if key not in self.dic:
+            return -1
+        v = self.dic.pop(key)
+        self.dic[key] = v
+        return v
+
+    def set(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: nothing
+        """
+        if key in self.dic:
+            self.dic.pop(key)
+        else:
+            if self.remain > 0:
+                self.remain -= 1
+            else:
+                self.dic.popitem(last=False)
+        self.dic[key] = value
+
+
+if __name__ == '__main__':
+    print 'h'
