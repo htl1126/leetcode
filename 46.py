34428702ac5f95bd9522a69902b97ab97fd554b2
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-05-11 15:16:52 -0400

    Create 46.py

---
+++ 46.py
@@ -0,0 +1,26 @@
+# ref: https://leetcode.com/discuss/18212/my-elegant-recursive
+#              -c-solution-with-inline-explanation
+
+
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, 0, res)
+        return res
+
+    def dfs(self, nums, begin, res):
+        if begin == len(nums):
+            res.append(nums[:])
+        else:
+            for i in xrange(begin, len(nums)):
+                nums[i], nums[begin] = nums[begin], nums[i]
+                self.dfs(nums, begin + 1, res)
+                nums[i], nums[begin] = nums[begin], nums[i]
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.permute([1, 2])
