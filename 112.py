7a28eee36f86884e94a0b90fd971e3fde074ff51
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-05-20 13:57:43 -0400

    Create 112.py

---
+++ 112.py
@@ -0,0 +1,38 @@
+# ref: https://leetcode.com/discuss/28873/short-python-recursive-solution-o-n
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right and root.val == sum:
+            return True
+        sum -= root.val
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(
+            root.right, sum)
+
+if __name__ == '__main__':
+    sol = Solution()
+    root = TreeNode(5)
+    root.left = TreeNode(4)
+    root.right = TreeNode(8)
+    root.left.left = TreeNode(11)
+    root.right.left = TreeNode(13)
+    root.right.right = TreeNode(4)
+    root.left.left.left = TreeNode(7)
+    root.left.left.right = TreeNode(2)
+    root.right.right.right = TreeNode(1)
+    print sol.hasPathSum(root, 22)
