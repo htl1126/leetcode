8fee16fc702047a04f34e1a2c9c8e327b4e3b99d
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-06-28 16:21:44 -0400

    Create 138.py

---
+++ 138.py
@@ -0,0 +1,32 @@
+# ref: https://leetcode.com/discuss/26815/clear-and-short-python-o-2n-and-o-n
+#              -solution
+import collections
+
+
+# Definition for singly-linked list with a random pointer.
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        dic = collections.defaultdict(lambda: RandomListNode(0))
+        dic[None] = None
+        n = head
+        while n:
+            dic[n].label = n.label
+            dic[n].next = dic[n.next]
+            dic[n].random = dic[n.random]
+            n = n.next
+        return dic[head]
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.copyRandomList()
