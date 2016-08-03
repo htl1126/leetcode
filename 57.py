5c1d3cb6468eb7d1d94ecc43bd3e72dd4558253a
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-03 09:20:43 -0400

    Create 57.py

---
+++ 57.py
@@ -0,0 +1,28 @@
+# ref: https://discuss.leetcode.com/topic/16988/7-lines-3-easy-solutions/2
+
+
+# Definition for an interval.
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[Interval]
+        :type newInterval: Interval
+        :rtype: List[Interval]
+        """
+        s, e = newInterval.start, newInterval.end
+        left, right = [], []
+        for i in intervals:
+            if i.end < s:
+                left += i,
+            elif i.start > e:
+                right += i,
+            else:
+                s = min(s, i.start)
+                e = max(e, i.end)
+        return left + [Interval(s, e)] + right
