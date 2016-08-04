9c4f38f2f2b4bf729bb581a2805414f18cbaed5d
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-04 15:57:00 -0400

    Update 168.py

--- 168.py
+++ 168.py
@@ -1,17 +1,17 @@
-# Solution Key: Convert a decimal number to a number of base 26
-# Time complexity: O(n), n is the number of digits of the resulting number
-# Space complexity: O(n), n is the number of digits of the resulting number
-class Solution:
-    # @return a string
-    def convertToTitle(self, num):
-        result = ""
-        num -= 1
-        while True:
-            if num < 0:
-                break
-            current_digit = num % 26
-            num = num / 26 - 1
-            current_alphabet = chr(ord('A') + current_digit)
-            result = "{0}{1}".format(result, current_alphabet)
-        result = result[::-1]
-        return result
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        ans = ''
+        n -= 1
+        alphabet = [chr(ord('A') + i) for i in xrange(26)]
+        while n >= 0:
+            ans += alphabet[n % 26]
+            n = n / 26 - 1
+        return ans[::-1]
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.convertToTitle(1)
