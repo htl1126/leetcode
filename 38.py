6f99c29c123a57dac6b0d957aa5283f548f213db
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-04 21:16:55 -0400

    Update 38.py

--- 38.py
+++ 38.py
@@ -1,21 +1,25 @@
-# use itertools.groupby() consice solution but too slow
-# source: https://leetcode.com/discuss/75204/4-5-lines-python-solutions
-
-import sys
-import itertools
-
 class Solution(object):
     def countAndSay(self, n):
         """
         :type n: int
         :rtype: str
         """
-        s = '1'
-        for _ in xrange(n - 1):
-            s = ''.join('{0}{1}'.format(len(list(group)), digit) for digit, group
-                        in itertools.groupby(s))
-        return s
+        n -= 1
+        ans = '1'
+        for i in xrange(n):
+            temp = ''
+            begin, end = 0, 1
+            while 1:
+                while end < len(ans) and ans[begin] == ans[end]:
+                    end += 1
+                temp += str(end - begin) + ans[begin]
+                if end >= len(ans):
+                    break
+                begin = end
+                end = begin + 1
+            ans = temp[:]
+        return ans
 
 if __name__ == '__main__':
     sol = Solution()
-    print sol.countAndSay(int(sys.argv[1]))
+    print sol.countAndSay(6)
