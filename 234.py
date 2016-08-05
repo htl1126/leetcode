34b392f2f95ca3e2b00871288ebbcb4d438238ed
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-05 11:24:42 -0400

    Update 234.py

--- 234.py
+++ 234.py
@@ -1,5 +1,6 @@
-# finding the second half of the list and reversing it is really tricky
-# source: https://leetcode.com/discuss/69366/short-python-solution
+# ref: https://discuss.leetcode.com/topic/18293/11-lines-12-with-restore-o-n-
+#              time-o-1-space
+
 
 # Definition for singly-linked list.
 class ListNode(object):
@@ -7,41 +8,30 @@ class ListNode(object):
         self.val = x
         self.next = None
 
+
 class Solution(object):
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        if not head or not head.next:
-            return True
-        prev = ListNode('')
-        prev.next = head
-        curr = head
-        fast = head
+        rev = None
+        slow = fast = head
         while fast and fast.next:
-            prev = prev.next
-            curr = curr.next
             fast = fast.next.next
-        prev.next = None
-        new_head = None
-        while curr:
-            tmp = curr.next
-            curr.next = new_head
-            new_head = curr
-            curr = tmp
-        while head and new_head:
-            if head.val != new_head.val:
-                return False
-            head = head.next
-            new_head = new_head.next
-        return True
-        
+            rev, rev.next, slow = slow, rev, slow.next
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            rev, slow = rev.next, slow.next
+        return not rev
+
 if __name__ == '__main__':
     sol = Solution()
     head = ListNode(1)
     head.next = ListNode(2)
     head.next.next = ListNode(3)
-    head.next.next.next = ListNode(2)
-    head.next.next.next.next = ListNode(1)
+    head.next.next.next = ListNode(3)
+    head.next.next.next.next = ListNode(2)
+    head.next.next.next.next.next = ListNode(1)
     print sol.isPalindrome(head)
