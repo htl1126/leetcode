803ff8f3a51d6b8d018b3486b2bfc395740294b4
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-09 19:32:41 -0400

    Create 380.py

---
+++ 380.py
@@ -0,0 +1,46 @@
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.data = {}
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.data:
+            return False
+        else:
+            self.data[val] = 1
+            return True
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.data:
+            self.data.pop(val)
+            return True
+        else:
+            return False
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        import random
+        return self.data.keys()[random.randint(0, len(self.data) - 1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
