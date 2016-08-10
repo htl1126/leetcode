ec00a98936c0e997bae24890ee533adc23dd7f29
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-10 07:56:55 -0400

    Update 43.py

--- 43.py
+++ 43.py
@@ -1,6 +1,5 @@
-# faster solution: https://leetcode.com/discuss/49026/share-my-60-ms-python-solution
-
 import sys
+
 
 class Solution(object):
     def multiply(self, num1, num2):
@@ -9,24 +8,21 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        atoi = lambda c: ord(c) - ord('0')
-        num1 = num1[::-1]
-        num2 = num2[::-1]
-        ans = [0] * (len(num1) + len(num2))
-        for i in xrange(len(num1)):
-            for j in xrange(len(num2)):
-                product = atoi(num1[i]) * atoi(num2[j])
-                ans[i + j] += product
-        ans.append(0)
-        for i in xrange(1, len(ans)):
-            ans[i] += ans[i - 1] / 10
-            ans[i - 1] %= 10
-        ans = ans[::-1]
-        for i in xrange(len(ans)):
+        num1 = [int(i) for i in num1][::-1]
+        num2 = [int(i) for i in num2][::-1]
+        len1, len2 = len(num1), len(num2)
+        ans = [0 for _ in xrange(len1 + len2)]
+        for i in xrange(len1):
+            for j in xrange(len2):
+                ans[i + j] += num1[i] * num2[j]
+        for i in xrange(len(ans) - 1):
+            ans[i + 1] += ans[i] / 10
+            ans[i] %= 10
+        for i in xrange(len(ans) - 1, -1, -1):
             if ans[i] != 0:
-                return ''.join([str(item) for item in ans[i:]])
+                return ''.join([str(i) for i in reversed(ans[:i + 1])])
         return '0'
-        
+
 if __name__ == '__main__':
     sol = Solution()
     print sol.multiply(sys.argv[1], sys.argv[2])
