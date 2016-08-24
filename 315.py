48a1310c323cf2845c2011e98313ac38c9fb6f8f
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-23 20:09:46 -0400

    Update 315.py

--- 315.py
+++ 315.py
@@ -11,6 +11,12 @@ class Solution(object):
             half = len(enum) / 2
             if half:
                 left, right = sort(enum[:half]), sort(enum[half:])
+                # All the numbers in two halves are sorted. We picked the
+                # bigger one in one half and placed it at enum[len(enum) - 1],
+                # enum[len(enum) - 2]..., etc. If we picked one from the left,
+                # we updated the number by the length of the right, because all
+                # the numbers in the right are less than the number picked in
+                # the left currently.
                 for i in xrange(len(enum) - 1, -1, -1):
                     if not right or left and left[-1][1] > right[-1][1]:
                         smaller[left[-1][0]] += len(right)
