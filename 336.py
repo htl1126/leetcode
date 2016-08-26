251a1d94e1cba3f115735217039e9664cfe52b59
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-25 21:05:20 -0400

    Create 336.py

---
+++ 336.py
@@ -0,0 +1,23 @@
+# ref: https://discuss.leetcode.com/topic/39526/python-solution
+
+
+class Solution(object):
+    def palindromePairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[int]]
+        """
+        d, res = dict([(w[::-1], i) for i, w in enumerate(words)]), []
+        for i, w in enumerate(words):
+            for j in xrange(len(w) + 1):
+                prefix, postfix = w[:j], w[j:]
+                if prefix in d and i != d[prefix] and postfix == postfix[::-1]:
+                    res.append([i, d[prefix]])
+                if j > 0 and postfix in d and i != d[postfix] and \
+                        prefix == prefix[::-1]:
+                    res.append([d[postfix], i])
+        return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.palindromePairs(["abcd", "dcba", "lls", "s", "sssll"])
