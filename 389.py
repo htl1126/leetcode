e4cd95a44a9e74bd2119670e6c110f0015031684
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-28 08:11:39 -0400

    Create 389.py

---
+++ 389.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        return (collections.Counter(t) - collections.Counter(s)).keys()[0]
