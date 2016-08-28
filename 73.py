57cce7d3f012e49b1d1f2d72f5d81ed677c4e5b8
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-28 06:38:15 -0400

    Create 73.py

---
+++ 73.py
@@ -0,0 +1,26 @@
+# ref: https://discuss.leetcode.com/topic/30043/java-python-o-1-space-11-lines
+#              -solution
+
+
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        row, col, FirstRowHasZero = len(matrix), len(matrix[0]), not all(
+            matrix[0])
+        for i in xrange(1, row):
+            for j in xrange(col):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+        for i in xrange(1, row):
+            for j in xrange(col - 1, -1, -1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if FirstRowHasZero:
+            matrix[0] = [0] * col
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.setZeroes([[1, 0, 1], [0, 1, 0]])
