4b858c89be215e6a2fe9be47cd9a1244bace1e8a
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-08-30 21:33:59 -0400

    Create 94.py

---
+++ 94.py
@@ -0,0 +1,31 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+
+if __name__ == '__main__':
+    sol = Solution()
+    root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+    print sol.inorderTraversal(root)
