aa39e739005fc33a60b5a1838c74faf5c74fd56f
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-01 07:47:06 -0400

    Create 109.py

---
+++ 109.py
@@ -0,0 +1,43 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def sortedListToBST(self, head):
+        """
+        :type head: ListNode
+        :rtype: TreeNode
+        """
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+        slow, fast, pre = head, head, None
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+        root = TreeNode(slow.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(slow.next)
+        return root
+
+if __name__ == '__main__':
+    sol = Solution()
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
+    root = sol.sortedListToBST(head)
+    print root.val, root.left.val, root.right.val, root.left.left.val, root.right.left.val
