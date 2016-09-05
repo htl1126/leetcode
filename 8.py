626028329d7ed55fab860aa2b8120986ac00e5f0
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-05 16:02:20 -0400

    Create 8.py

--- 8.py
+++ 8.py
@@ -1,17 +1,20 @@
 # ad-hoc
 
+
 class Solution(object):
     def myAtoi(self, str):
         """
         :type str: str
         :rtype: int
         """
-        if len(str) == 0: return 0
+        if not str:
+            return 0
         str = list(str.strip())
-        sign = -1 if str[0] == '-' else 1
-        if str[0] in ['+', '-']: del str[0]
+        sign = [1, -1][str[0] == '-']
+        if str[0] in '-+':
+            del str[0]
         ans, i = 0, 0
-        while i < len(str) and str[i].isdigit():
+        while i < len(str) and '0' <= str[i] <= '9':
             ans = ans * 10 + ord(str[i]) - ord('0')
             i += 1
-        return max(-2 ** 31, min(sign * ans, 2 ** 31 - 1))
+        return min(2 ** 31 - 1, max(ans * sign, - 2 ** 31))
