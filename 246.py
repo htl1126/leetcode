0052fdd0d4bfca9f0831840b7b6589ee198f8490
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-07 10:43:00 -0400

    Update 246.py

--- 246.py
+++ 246.py
@@ -7,13 +7,13 @@ class Solution(object):
         :type num: str
         :rtype: bool
         """
-        dic = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
+        d = {'0': '0', '1': '1', '6': '9', '8': '8', '9': '6'}
         l, r = 0, len(num) - 1
         while l <= r:
-            if num[l] not in dic or dic[num[l]] != num[r]:
+            if num[l] in d and d[num[l]] == num[r]:
+                l, r = l + 1, r - 1
+            else:
                 return False
-            l += 1
-            r -= 1
         return True
 
 if __name__ == '__main__':
