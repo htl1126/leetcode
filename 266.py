c96a131c42015bd0952900b3ffede782671d5d6c
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-07 10:11:42 -0400

    Create 266.py

--- 266.py
+++ 266.py
@@ -1,27 +1,14 @@
+import collections
+
+
 class Solution(object):
     def canPermutePalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        freq = {}
-        for c in s:
-            if c in freq:
-                freq[c] += 1
-            else:
-                freq[c] = 1
-        freq_lst = freq.values()
-        freq_lst = [item % 2 for item in freq_lst]
-        if len(s) % 2 == 0:
-            if sum(freq_lst)  == 0:
-                return True
-            else:
-                return False
-        else:
-            if sum(freq_lst) == 1:
-                return True
-            else:
-                return False
+        ctr = collections.Counter(s)
+        return len([v for v in ctr.values() if v & 1]) <= 1
 
 if __name__ == '__main__':
     sol = Solution()
