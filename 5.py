80a244332764b547a07f6146d27c939859d8c611
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-07 21:10:55 -0400

    Update 5.py

--- 5.py
+++ 5.py
@@ -15,7 +15,7 @@ class Solution(object):
             if i - maxlen - 1 >= 0 and \
                     s[i - maxlen - 1:i + 1] == s[i - maxlen - 1:i + 1][::-1]:
                 start = i - maxlen - 1
-                maxlen += 1
+                maxlen += 2
             if i - maxlen >= 0 and \
                     s[i - maxlen:i + 1] == s[i - maxlen:i + 1][::-1]:
                 start = i - maxlen
