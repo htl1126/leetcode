ac18dc3c98839640a8b47c64f299db8f98eee72c
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-16 10:24:26 -0400

    Create 144.py

---
+++ 144.py
@@ -0,0 +1,35 @@
+# ref: https://discuss.leetcode.com/topic/21281/python-solutions-recursively-and
+#              -iteratively
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        stack, res = [root], []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return res
+
+if __name__ == '__main__':
+    sol = Solution()
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    print sol.preorderTraversal(root)
