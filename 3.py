fb2522ba1e27af2584f0e69c01dbb86847fd5560
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-20 11:51:51 -0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,8 @@
-import sys
-
 # ref: https://leetcode.com/discuss/64749/my-python
 #      -solution-with-o-n-time-and-o-1-space
 # keep track of last occurrence for each alphabet
 # update longest sequence length when facing a new alphabet
+
 
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
@@ -11,20 +10,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if len(s) == 0:
+        if not s:
             return 0
-        str_len = len(s)
-        max_len = 1
-        begin = 0
-        last_pos = {s[0]: 0}
+        str_len, max_len, begin, d = len(s), 1, 0, {s[0]: 0}
         for i in xrange(1, str_len):
-            if s[i] in last_pos and last_pos[s[i]] >= begin:
-                begin = last_pos[s[i]] + 1
-            else:
-                max_len = max(max_len, i - begin + 1)
-            last_pos[s[i]] = i
+            if s[i] in d and d[s[i]] >= begin:
+                begin = d[s[i]] + 1
+            max_len = max(max_len, i - begin + 1)
+            d[s[i]] = i
         return max_len
-        
+
 if __name__ == '__main__':
     sol = Solution()
-    print sol.lengthOfLongestSubstring(sys.argv[1])
+    print sol.lengthOfLongestSubstring('pwwkew')
