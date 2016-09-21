159ac9f6caa0c652d279fabc5eee3e5ea3c56797
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-21 12:53:28 -0400

    Update 159.py

--- 159.py
+++ 159.py
@@ -1,6 +1,5 @@
-# ref: https://discuss.leetcode.com/topic/11739/clean-11-lines-ac-answer-o-1
-#              -space-o-n-time
-# Follow-up: how about three distinct?
+# ref: https://discuss.leetcode.com/topic/7399/share-my-c-solution
+import collections
 
 
 class Solution(object):
@@ -9,15 +8,19 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        i, j, maxlen = 0, -1, 0
-        for k in xrange(1, len(s)):
-            if s[k] != s[k - 1]:
-                if j >= 0 and s[k] != s[j]:
-                    maxlen = max(maxlen, k - i)
-                    i = j + 1  # the new begin position for the new sequence
-                j = k - 1
-        return maxlen if maxlen > len(s) - i else len(s) - i  # case 'eebbeee'
-
+        j, d, maxlen, count = -1, collections.defaultdict(int), 0, 0
+        for i in xrange(len(s)):
+            d[s[i]] += 1
+            if d[s[i]] == 1:
+                count += 1
+            while count > 2:
+                j += 1
+                d[s[j]] -= 1
+                if not d[s[j]]:
+                    del d[s[j]]
+                    count -= 1
+            maxlen = max(maxlen, i - j)
+        return maxlen
 
 if __name__ == '__main__':
     sol = Solution()
