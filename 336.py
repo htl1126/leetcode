f015a57e30ea07ae9e2ccc48aab712ffbf83a1ce
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-21 09:08:13 -0400

    Update 336.py

--- 336.py
+++ 336.py
@@ -11,8 +11,11 @@ class Solution(object):
         for i, w in enumerate(words):
             for j in xrange(len(w) + 1):
                 prefix, postfix = w[:j], w[j:]
+                # prefix + postfix + prefix[::-1] is a palindrome
                 if prefix in d and i != d[prefix] and postfix == postfix[::-1]:
                     res.append([i, d[prefix]])
+                # postfix[::-1] + prefix + postfix is a palindrome, 'j > 0'
+                # prevents empty prefix
                 if j > 0 and postfix in d and i != d[postfix] and \
                         prefix == prefix[::-1]:
                     res.append([d[postfix], i])
