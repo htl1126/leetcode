6869598e9b2d78a6636dabd3d3684272d14e7d19
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-22 17:26:46 -0400

    Update 37.py

--- 37.py
+++ 37.py
@@ -8,6 +8,7 @@ class Solution(object):
                          dict((i, set()) for i in xrange(1, 10)),
                          dict((i, set()) for i in xrange(1, 10)))
         empty = []
+        self.found = False
         for i in xrange(9):
             for j in xrange(9):
                 if board[i][j] != '.':
@@ -22,11 +23,13 @@ class Solution(object):
             if step == n:
                 for idx, (i, j) in enumerate(empty):
                     board[i][j] = str(cur_ans[idx])
+                self.found = True
             else:
                 i, j = empty[step]
                 for v in xrange(1, 10):
                     if i not in row[v] and j not in col[v] and \
-                            (i / 3) * 3 + j / 3 not in box[v]:
+                            (i / 3) * 3 + j / 3 not in box[v] and \
+                            not self.found:
                         row[v].add(i)
                         col[v].add(j)
                         box[v].add((i / 3) * 3 + j / 3)
