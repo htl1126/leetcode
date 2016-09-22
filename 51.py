552b3abd0ad3678e9a03f0a02137945cbf2079bc
Tian-Liang Huang <b93902098@ntu.edu.tw>
2016-09-22 09:41:59 -0400

    Update 51.py

--- 51.py
+++ 51.py
@@ -1,3 +1,7 @@
+# ref: https://discuss.leetcode.com/topic/20217/fast-short-and-easy-to
+#              -understand-python-solution-11-lines-76ms
+
+
 class Solution(object):
     def solveNQueens(self, n):
         """
@@ -6,22 +10,17 @@ class Solution(object):
         """
         sol = []
 
-        def get_sol(q_pos, na, depth):
+        def get_sol(q_pos, xy_sum, xy_dif, depth):
             if depth == n:
-                sol.append([''.join('Q' if (i, j) in q_pos else '.'
-                                    for j in xrange(n)) for i in xrange(n)])
+                sol.append(['.' * q_pos[i] + 'Q' + '.' * (n - q_pos[i] - 1)
+                            for i in xrange(n)])
             else:
                 for j in xrange(n):
-                    if (depth, j) not in na:
-                        get_sol(q_pos | {(depth, j)}, na |
-                                {(x, j) for x in xrange(n)} |
-                                {(depth, x) for x in xrange(n)} |
-                                {(x, depth + j - x) for x in xrange(n)
-                                 if 0 <= x < n and 0 <= depth + j - x < n} |
-                                {(x, x - (depth - j)) for x in xrange(n)
-                                 if 0 <= x < n and 0 <= x - (depth - j) < n},
-                                depth + 1)
-        get_sol(set(), set(), 0)
+                    if j not in q_pos and depth + j not in xy_sum and \
+                            depth - j not in xy_dif:
+                        get_sol(q_pos + [j], xy_sum | {depth + j}, xy_dif |
+                                {depth - j}, depth + 1)
+        get_sol([], set(), set(), 0)
         return sol
 
 if __name__ == '__main__':
