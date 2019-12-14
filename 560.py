a7d55aa9a92b5ecf95dfbbbe4dc4d9b21197d1ed
Tian-Liang Huang <thuang@mediamath.com>
2019-12-13 22:59:14 -0800

    Create 560.py

---
+++ 560.py
@@ -0,0 +1,24 @@
+import collections
+
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = collections.Counter()
+        ans = Sum = 0
+        count[0] = 1  # edge case, used for count[0] + ... + count[n - 1] = K
+        for i in xrange(len(nums)):
+            Sum += nums[i]
+            ans += count[Sum - k]
+            count[Sum] += 1  # found another subsequence, so add one
+        return ans
+
+if __name__ == "__main__":
+    sol = Solution()
+    print sol.subarraySum([1, 1, 1], 2)
+
+# Algorithm: dynamic programming
+# Ref: https://leetcode.com/problems/subarray-sum-equals-k/discuss/102111/Python-Simple-with-Explanation
