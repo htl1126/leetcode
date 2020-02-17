eb0b12d255d1782a79d1dcfa774da26ef1803ad4
htl1126 <tlhuang@tlhuang.net>
2020-02-17 13:27:26 -0800

    Create 1060.py

---
+++ 1060.py
@@ -0,0 +1,24 @@
+# Ref: https://leetcode.com/problems/missing-element-in-sorted-array/discuss/305579/C%2B%2B-binary-search
+# Ref: https://leetcode.com/problems/missing-element-in-sorted-array/discuss/307872/python-binary-search
+# Algo: binary search
+
+class Solution(object):
+    def missingElement(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r + 1) / 2
+            if nums[mid] - nums[0] - mid >= k:
+                r = mid - 1
+            else:
+                l = mid
+        return k + nums[0] + l
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print sol.missingElement([4, 7, 9, 10], 3)
