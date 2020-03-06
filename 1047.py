cc3e6d44b18a16726c99492df79a6491458cc700
htl1126 <tlhuang@tlhuang.net>
2020-03-05 21:34:16 -0500

    Create 1047.py

---
+++ 1047.py
@@ -0,0 +1,20 @@
+# Ref: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/discuss/294893/JavaC%2B%2BPython-Two-Pointers-and-Stack-Solution
+# Algo: stack
+
+class Solution(object):
+    def removeDuplicates(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        stack = []
+        for c in S:
+            if stack and stack[-1] == c:
+                stack.pop()
+            else:
+                stack.append(c)
+        return ''.join(stack)
+
+if __name__ == "__main__":
+    sol = Solution()
+    print sol.removeDuplicates("abbaca")
