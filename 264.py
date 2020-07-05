dacb75c1cde3b4340633aed416f08e509941925e
htl1126 <tlhuang@tlhuang.net>
2020-07-05 00:02:16 -0400

    Modified 264.py

--- 264.py
+++ 264.py
@@ -1,23 +1,11 @@
-import heapq
-
-
 class Solution(object):
+    ugly = sorted(2 ** a * 3 ** b * 5 ** c for a in xrange(32) for b in xrange(20) for c in xrange(14))
     def nthUglyNumber(self, n):
         """
         :type n: int
         :rtype: int
         """
-        uglies = [1]
-
-        def gen(prime):
-            for ugly in uglies:
-                yield ugly * prime
-        merged = heapq.merge(*map(gen, [2, 3, 5]))
-        while len(uglies) < n:
-            ugly = merged.next()
-            if ugly != uglies[-1]:
-                uglies += ugly,
-        return uglies[-1]
+        return self.ugly[n - 1] 
 
 if __name__ == '__main__':
     sol = Solution()
