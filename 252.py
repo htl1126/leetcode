483b29201d1640f72d56d30f0c70bae4218efd6d
htl1126 <tlhuang@tlhuang.net>
2020-11-01 12:36:45 -0500

    Modified 252.py

--- 252.py
+++ 252.py
@@ -1,26 +1,15 @@
-# Definition for an interval.
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-
-
 class Solution(object):
     def canAttendMeetings(self, intervals):
         """
-        :type intervals: List[Interval]
+        :type intervals: List[List[int]]
         :rtype: bool
         """
-        if len(intervals) < 2:
-            return True
-        else:
-            intervals = sorted(intervals, key=lambda x: x.start)
-            for i in xrange(len(intervals) - 1):
-                if intervals[i].end > intervals[i + 1].start:
-                    return False
-            return True
+        intervals = sorted(intervals)
+        for i in xrange(len(intervals) - 1):
+           if intervals[i][1] > intervals[i + 1][0]:
+               return False
+        return True
 
 if __name__ == '__main__':
     sol = Solution()
-    print sol.canAttendMeetings([Interval(0, 30), Interval(5, 10),
-                                 Interval(7, 20)])
+    print sol.canAttendMeetings([[0, 30], [5, 10], [15, 20]])
