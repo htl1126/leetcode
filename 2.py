dac814a9795ffe1b173c548c9828a9f63755b991
htl1126 <tlhuang@tlhuang.net>
2021-01-12 13:40:00 -0500

    Modified 2.py

--- 2.py
+++ 2.py
@@ -12,27 +12,21 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        if not l1 and not l2:
-            return None
-        if not l1:
-            return l2
-        if not l2:
-            return l1
-        carry = 0
-        ans = head = ListNode(None)
+        ans = ListNode(0)
+        pre = prehead = ListNode(next=ans)
         while l1 or l2:
-            ans.next = ListNode(0)
-            ans = ans.next
             if l1:
                 ans.val += l1.val
                 l1 = l1.next
             if l2:
                 ans.val += l2.val
                 l2 = l2.next
-            ans.val += carry
-            carry = ans.val >= 10
-            if carry:
+            if ans.val >= 10:
                 ans.val -= 10
-        if carry:
-            ans.next = ListNode(1)
-        return head.next
+                ans.next = ListNode(val=1)
+            else:
+                ans.next = ListNode(val=0)
+            pre, ans = ans, ans.next
+        if ans.val == 0:
+            pre.next = None
+        return prehead.next
