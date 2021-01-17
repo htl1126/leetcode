2ccee9ff8ed5c737a906bf87d46d25a2df93288c
htl1126 <tlhuang@tlhuang.net>
2021-01-17 16:49:35 -0500

    Modified 215.py

--- 215.py
+++ 215.py
@@ -1,31 +1,28 @@
 # ref: https://leetcode.com/discuss/53530/python-different-solutions-
 #              comments-bubble-selection-quick
 
+import random
 
-class Solution(object):
-    def findKthLargest(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        if nums:
-            pos = self.partition(nums, 0, len(nums) - 1)
-            if k > pos + 1:
-                return self.findKthLargest(nums[pos + 1:], k - (pos + 1))
-            elif k < pos + 1:
-                return self.findKthLargest(nums[:pos], k)
-            else:
-                return nums[pos]
-
-    def partition(self, nums, l, r):
-        low = l
-        while l < r:
-            if nums[l] > nums[r]:
-                nums[low], nums[l] = nums[l], nums[low]
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        pos = self.partition(nums, 0, len(nums) - 1)
+        if pos == k - 1:
+            return nums[pos]
+        elif pos < k - 1:
+            return self.findKthLargest(nums[pos + 1:], k - pos - 1)
+        else:
+            return self.findKthLargest(nums[:pos], k)
+        
+    def partition(self, nums, lo, hi):
+        pivot = random.randint(lo, hi)
+        nums[pivot], nums[hi] = nums[hi], nums[pivot]
+        low, l = lo, lo
+        while l < hi:
+            if nums[l] > nums[hi]:
+                nums[l], nums[low] = nums[low], nums[l]
                 low += 1
             l += 1
-        nums[low], nums[r] = nums[r], nums[low]
+        nums[low], nums[hi] = nums[hi], nums[low]
         return low
 
 if __name__ == '__main__':
