c88e307b3c6b3b613a7faedfa10873dc33134373
htl1126 <tlhuang@tlhuang.net>
2021-01-21 00:00:23 -0500

    Modified 20.py

--- 20.py
+++ 20.py
@@ -4,16 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = list(s)
         stack = []
-        dic = {')': '(', ']': '[', '}': '{'}
-        for char in s:
-            if char in ['(', '[', '{']:
-                stack.append(char)
-            elif char in dic:
-                if not stack or stack[-1] != dic[char]:
-                    return False
+        dic = {'(': ')', '[': ']', '{': '}'}
+        for c in s:
+            if stack and stack[-1] in "([{" and dic[stack[-1]] == c:
                 stack.pop()
+            else:
+                stack.append(c)
         return not stack
 
 if __name__ == '__main__':
