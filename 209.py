e639fb9cd1a946bfeb37dfd509b9c331e9319340
htl1126 <tlhuang@tlhuang.net>
2021-03-02 17:39:05 -0500

    Modified 209.py

--- 209.py
+++ 209.py
@@ -1,23 +1,16 @@
 # ref: https://discuss.leetcode.com/topic/14288/python-o-n-and-o-n
 #              -log-n-solution
 
-
-class Solution(object):
-    def minSubArrayLen(self, s, nums):
-        """
-        :type s: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        result, total, l, r = len(nums) + 1, 0, 0, 0
-        for idx, num in enumerate(nums):
-            total += num
-            r = idx
-            while total >= s:
-                result = min(result, r - l + 1)
-                total -= nums[l]
-                l += 1
-        return result if result <= len(nums) else 0
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        cur, i, ans = 0, 0, len(nums) + 1
+        for j in range(len(nums)):
+            cur += nums[j]
+            while cur >= target:
+                ans = min(ans, j - i + 1)
+                cur -= nums[i]
+                i += 1
+        return ans % (len(nums) + 1)
 
 if __name__ == '__main__':
     sol = Solution()
