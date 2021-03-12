5a43807d9d2b5adb0155158ff36b1640fbd54467
htl1126 <tlhuang@tlhuang.net>
2021-03-12 01:13:02 -0500

    Created 721.py

---
+++ 721.py
@@ -0,0 +1,26 @@
+# Ref: https://leetcode.com/problems/accounts-merge/discuss/109161/Python-Simple-DFS-with-explanation!!!
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        email_account_map = collections.defaultdict(list)
+        visited = [False] * len(accounts)
+        res = []
+        for i, account_lst in enumerate(accounts):
+            for j in range(1, len(account_lst)):
+                email_account_map[account_lst[j]].append(i)
+                
+        def dfs(i, emails):
+            if not visited[i]:
+                visited[i] = True
+                for j in range(1, len(accounts[i])):
+                    email = accounts[i][j]
+                    emails.add(email)
+                    for k in email_account_map[email]:
+                        dfs(k, emails)
+                        
+        for i, account in enumerate(accounts):
+            if not visited[i]:
+                name, emails = account[0], set()
+                dfs(i, emails)
+                res.append([name] + sorted(emails))
+        return res
