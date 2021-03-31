2b5bcc1cba5f0ef5a337c14866ce2c25f648482f
htl1126 <tlhuang@tlhuang.net>
2021-03-31 00:03:49 -0400

    Modified 354.py

--- 354.py
+++ 354.py
@@ -1,36 +1,13 @@
-# ref: https://discuss.leetcode.com/topic/48160/python-o-nlogn-o-n-solution
-#              -beats-97-with-explanation
+# ref: https://leetcode.com/problems/russian-doll-envelopes/discuss/1134197/Python-4-lines-solution-explained
 
 
 class Solution(object):
     def maxEnvelopes(self, envelopes):
-        """
-        :type envelopes: List[List[int]]
-        :rtype: int
-        """
-        def liss(envs):
-            def limp(envs, tails, k):
-                begin, end = 0, len(tails) - 1
-                while begin <= end:
-                    mid = (begin + end) >> 1
-                    if envs[tails[mid]][1] >= k[1]:
-                        end = mid - 1
-                    else:
-                        begin = mid + 1
-                return begin
-            tails = []
-            for i, env in enumerate(envs):
-                idx = limp(envs, tails, env)
-                if idx >= len(tails):
-                    tails.append(i)
-                else:
-                    tails[idx] = i
-            return len(tails)
-
-        def f(x, y):
-            return -1 if (x[0] < y[0] or x[0] == y[0] and x[1] > y[1]) else 1
-        envelopes.sort(cmp=f)
-        return liss(envelopes)
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        dp = [float('inf')] * (len(envelopes) + 1)
+        for env in envelopes:
+            dp[bisect.bisect_left(dp, env[1])] = env[1]
+        return dp.index(float('inf'))
 
 if __name__ == '__main__':
     sol = Solution()
