318fc7bb855f4f019fd6eacb53f68d0cd11fbbe4
htl1126 <tlhuang@tlhuang.net>
2021-04-19 17:16:43 -0400

    Modified 377.py

--- 377.py
+++ 377.py
@@ -1,25 +1,17 @@
 # ref: https://discuss.leetcode.com/topic/52227/7-liner-in-python-and-follow-up
-#              -question
+#              -question
 
 
 class Solution(object):
-    def combinationSum4(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        nums, combs = sorted(nums), [1] + [0 for _ in xrange(target)]
-        for i in xrange(target + 1):
-            for num in nums:
-                if num > i:
-                    break
-                if num == i:
-                    combs[i] += 1
-                if num < i:
-                    combs[i] += combs[i - num]
-        return combs[-1]
+    def combinationSum4(self, nums, target):
+        nums, combs = sorted(nums), [1] + [0 for _ in range(target)]
+        for i in range(target + 1):
+            for num in nums:
+                if num > i:
+                    break
+                combs[i] += combs[i - num]
+        return combs[-1]
 
 if __name__ == '__main__':
-    sol = Solution()
-    print sol.combinationSum4([1, 2, 3], 4)
+    sol = Solution()
+    print sol.combinationSum4([1, 2, 3], 4)
