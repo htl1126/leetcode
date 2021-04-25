d705c2612882b431f01766c9368be01eaa162d31
htl1126 <tlhuang@tlhuang.net>
2021-04-25 17:36:45 -0400

    Created 510.py

---
+++ 510.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def inorderSuccessor(self, node: 'Node') -> 'Node':
+        if node.right:
+            node = node.right
+            while node.left:
+                node = node.left
+            return node
+        else:
+            while node.parent:
+                parent = node.parent
+                if node == parent.left:
+                    return parent
+                node = parent
+            return None
