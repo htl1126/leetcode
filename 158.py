851474faaa6df42e7013fa96348d5c5ca702c38d
htl1126 <tlhuang@tlhuang.net>
2021-04-25 11:42:44 -0400

    Modified 158.py

--- 158.py
+++ 158.py
@@ -1,32 +1,24 @@
 # ref: https://discuss.leetcode.com/topic/8795/my-python-40ms-solution/5
 
 # The read4 API is already defined for you.
-# @param buf, a list of characters
-# @return an integer
-# def read4(buf):
+# def read4(buf4: List[str]) -> int:
 
-
-class Solution(object):
-    def init(self):
-        self.queue = []
-
-    def read(self, buf, n):
-        """
-        :type buf: Destination buffer (List[str])
-        :type n: Maximum number of characters to read (int)
-        :rtype: The number of characters read (int)
-        """
-        tmpN = n
+class Solution:
+    def __init__(self):
+        self.c_buf = []
+    
+    def read(self, buf: List[str], n: int) -> int:
         ans = 0
-        while tmpN > 0:
-            tmpBuf = ['' for _ in xrange(4)]
-            read4(tmpBuf)
-            self.queue += tmpBuf
-            read_len = min(len(self.queue), n - ans)
-            for i in xrange(read_len):
-                buf[ans + i] = self.queue.pop(0)
-            tmpN -= read_len
-            ans += read_len
+        while n:
+            buf4 = [""] * 4
+            l = read4(buf4)
+            if l:
+                self.c_buf += buf4[:l]
+            read_len = min(len(self.c_buf), n)
+            for _ in range(read_len):
+                buf[ans] = self.c_buf.pop(0)
+                ans += 1
+                n -= 1
             if read_len < 4:
                 break
         return ans
