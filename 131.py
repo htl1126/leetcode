f4617e45b86cba25b5f42b6b5dd73be684618c5c
htl1126 <tlhuang@tlhuang.net>
2021-04-26 10:48:52 -0400

    Modified 131.py

--- 131.py
+++ 131.py
@@ -3,24 +3,18 @@
 
 
 class Solution(object):
-    def partition(self, s):
-        """
-        :type s: str
-        :rtype: List[List[str]]
-        """
+    def partition(self, s: str) -> List[List[str]]:
         if not s:
             return [[]]
         ans = {0: [[]], 1: [[s[0]]]}
-        for i in xrange(1, len(s)):
+        for i in range(1, len(s)):
             ans[i + 1] = []
-            for j in xrange(i + 1):
-                if self.isPalindrome(s[j:i + 1]):
+            for j in range(i + 1):
+                t = s[j:i + 1]
+                if t == t[::-1]:
                     for sol in ans[j]:
-                        ans[i + 1].append(sol + [s[j:i + 1]])
+                        ans[i + 1].append(sol + [t])
         return ans[len(s)]
-
-    def isPalindrome(self, s):
-        return s == s[::-1]
 
 if __name__ == '__main__':
     sol = Solution()
