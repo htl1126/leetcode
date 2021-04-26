7d421d029d975f4262b4c0e91a82d41843a45591
htl1126 <tlhuang@tlhuang.net>
2021-04-25 23:41:41 -0400

    Modified 424.py

--- 424.py
+++ 424.py
@@ -2,14 +2,13 @@
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        res = maxfreq = 0
+        res = maxfreq = 0  # res is the current longest result sequence size up to index i
         count = collections.Counter()
         for i in range(len(s)):
             count[s[i]] += 1
             maxfreq = max(maxfreq, count[s[i]])
-            if res < maxfreq + k:
+            if res - maxfreq < k:
                 res += 1
             else:
                 count[s[i - res]] -= 1
         return res
-
