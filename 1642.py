c41a435f93deadd9a9406bd688dc72fb8772b75b
htl1126 <tlhuang@tlhuang.net>
2021-04-27 01:56:44 -0400

    Created 1642.py

---
+++ 1642.py
@@ -0,0 +1,14 @@
+# Ref: https://leetcode.com/problems/furthest-building-you-can-reach/discuss/918515/JavaC%2B%2BPython-Priority-Queue
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        heap = []
+        for i in range(len(heights) - 1):
+            d = heights[i + 1] - heights[i]
+            if d > 0:
+                heapq.heappush(heap, d)
+            if len(heap) > ladders:
+                bricks -= heapq.heappop(heap)
+            if bricks < 0:
+                return i
+        return len(heights) - 1
