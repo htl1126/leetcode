8585bdec58b42c26acb45482fc7e8ea100ad457f
htl1126 <tlhuang@tlhuang.net>
2021-04-30 01:51:54 -0400

    Modified 220.py

--- 220.py
+++ 220.py
@@ -2,30 +2,22 @@
 #              -n-time-o-n-space-using-buckets
 
 
-class Solution(object):
-    def containsNearbyAlmostDuplicate(self, nums, k, t):
-        """
-        :type nums: List[int]
-        :type k: int
-        :type t: int
-        :rtype: bool
-        """
-        if t < 0:
-            return False
-        d = {}
-        w = t + 1
-        for i in xrange(len(nums)):
-            b = nums[i] / w
-            if b in d:
-                return True
-            if b + 1 in d and abs(d[b + 1] - nums[i]) < w:
-                return True
-            if b - 1 in d and abs(d[b - 1] - nums[i]) < w:
-                return True
-            d[b] = nums[i]
-            if i >= k:
-                del d[nums[i - k] / w]
-        return False
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
+        d = {}
+        w = t + 1
+        for i in range(len(nums)):
+            m = nums[i] // w
+            if m in d:
+                return True
+            if m - 1 in d and abs(d[m - 1] - nums[i]) <= t:
+                return True
+            if m + 1 in d and abs(d[m + 1] - nums[i]) <= t:
+                return True
+            d[m] = nums[i]
+            if i >= k:
+                del d[nums[i - k] // w]
+        return False
 
 if __name__ == '__main__':
     sol = Solution()
