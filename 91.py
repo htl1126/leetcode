0fc545b1def42e73c99566cbb53c2cdd15c20ad4
htl1126 <tlhuang@tlhuang.net>
2021-04-30 01:53:27 -0400

    Modified 91.py

--- 91.py
+++ 91.py
@@ -1,27 +1,12 @@
-# ref: https://leetcode.com/discuss/8527/dp-solution-java-for-reference
-
-
-class Solution(object):
-    def numDecodings(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if s == '':
-            return 0
-        table = [0 for _ in xrange(len(s) + 1)]
-        table[len(s)] = 1
-        if s[-1] == '0':
-            table[-2] = 0
-        else:
-            table[-2] = 1
-        for i in xrange(len(s) - 2, -1, -1):
-            if s[i] != '0':
-                if int(s[i: i + 2]) <= 26:
-                    table[i] = table[i + 1] + table[i + 2]
-                else:
-                    table[i] = table[i + 1]
-        return table[0]
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [1] + [0] * len(s)
+        for i in range(len(s)):
+            if 1 <= int(s[i]) <= 9:
+                dp[i + 1] += dp[i]
+            if i - 1 >= 0 and 10 <= int(s[i - 1:i + 1]) <= 26:
+                dp[i + 1] += dp[i - 1]
+        return dp[-1]
 
 if __name__ == '__main__':
     sol = Solution()
