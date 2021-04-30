73ea382dc0f129c795cbf42fe91642d200b835e8
htl1126 <tlhuang@tlhuang.net>
2021-04-30 01:52:16 -0400

    Modified 29.py

--- 29.py
+++ 29.py
@@ -4,34 +4,24 @@
 
 import sys
 
-class Solution(object):
-    def divide(self, dividend, divisor):
-        """
-        :type dividend: int
-        :type divisor: int
-        :rtype: int
-        """
-        max_int = 2147483647
-        if dividend == 0:
-            return 0
-        if divisor == 0:
-            return max_int
-        is_negative = (dividend > 0) ^ (divisor > 0)
-        dividend, divisor = abs(dividend), abs(divisor)
-        ans = 0
-        t = divisor
-        q = 1
-        while dividend >= t:
-            t <<= 1
-            q <<= 1
-        while t >= divisor:
-            if dividend >= t:
-                dividend -= t
-                ans += q
-            t >>= 1
-            q >>= 1
-        ans = -ans if is_negative else ans
-        return min(ans, max_int)
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == 0:
+            return 0
+        is_neg = (dividend > 0) ^ (divisor > 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        ans, t, q = 0, divisor, 1
+        while t <= dividend:
+            t <<= 1
+            q <<= 1
+        while t >= divisor:
+            if dividend >= t:
+                dividend -= t
+                ans += q
+            t >>= 1
+            q >>= 1
+        ans = -ans if is_neg else ans
+        return min(2 ** 31 - 1, max(ans, -2 ** 31))
 
 if __name__ == '__main__':
     sol = Solution()
