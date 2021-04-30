0d1a241164b565263b38ae591f587cea34e2f39b
htl1126 <tlhuang@tlhuang.net>
2021-04-30 17:22:09 -0400

    Modified 152.py

--- 152.py
+++ 152.py
@@ -2,24 +2,16 @@
 #              running-time
 
 
-class Solution(object):
-    def maxProduct(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if nums == []:
-            return 0
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
         maxherepre, minherepre, maxsofar = nums[0], nums[0], nums[0]
         maxhere, minhere = None, None
-        for i in xrange(1, len(nums)):
-            maxhere = max(max(maxherepre * nums[i], minherepre * nums[i]),
-                          nums[i])
-            minhere = min(min(maxherepre * nums[i], minherepre * nums[i]),
-                          nums[i])
+        for i in range(1, len(nums)):
+            n = nums[i]
+            maxhere = max(maxherepre * n, minherepre * n, n)
+            minhere = min(maxherepre * n, minherepre * n, n)
             maxsofar = max(maxhere, maxsofar)
-            maxherepre = maxhere
-            minherepre = minhere
+            maxherepre, minherepre = maxhere, minhere
         return maxsofar
 
 if __name__ == '__main__':
