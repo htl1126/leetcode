5589b394b54cc44cd7b3160e78e2399fcbf69383
htl1126 <tlhuang@tlhuang.net>
2021-05-02 13:29:22 -0400

    Modified 393.py

--- 393.py
+++ 393.py
@@ -1,23 +1,28 @@
-class Solution(object):
-    def validUtf8(self, data):
-        """
-        :type data: List[int]
-        :rtype: bool
-        """
-        n, i = len(data), 0
-        while i < n:
-            t = data[i]
-            sub_s_len = 1
-            if t & 128:
-                while t & 128:
-                    t, sub_s_len = t << 1, sub_s_len + 1
-                sub_s_len -= 1
-            if sub_s_len > n - i or sub_s_len == 1 and (data[i] & 128):
-                return False
-            if any(j for j in xrange(i + 1, i + sub_s_len)
-                   if (data[j] >> 6) & 3 != 2):
-                return False
-            i = i + sub_s_len
+class Solution:
+    def validUtf8(self, data: List[int]) -> bool:
+        num_byte_mask = {1: 1 << 1}
+        size = len(data)
+        for i in range(2, 5):
+            num_byte_mask[i] = (num_byte_mask[i - 1] | 1) << 1
+        i = 0
+        while i < size:
+            if data[i] >> 7 == 0:
+                i += 1
+            else:
+                if data[i] >> 6 == num_byte_mask[1]:
+                    return False
+                j = 2
+                max_utf8_size = min(4, size - i)
+                while j <= max_utf8_size:
+                    if num_byte_mask[j] == (data[i] >> (7 - j)):
+                        break
+                    j += 1
+                if j > max_utf8_size:
+                    return False
+                for k in range(i + 1, i + j):
+                    if (data[k] >> 6) & 2 != 2:
+                        return False
+                i += j
         return True
 
 if __name__ == '__main__':
