1038de43d176467d95b763d3afabee7dcd42d6fe
htl1126 <tlhuang@tlhuang.net>
2021-05-02 15:11:26 -0400

    Modified 333.py

--- 333.py
+++ 333.py
@@ -9,19 +9,15 @@ class TreeNode(object):
         self.right = None
 
 
-class Solution(object):
-    def largestBSTSubtree(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
+class Solution:
+    def largestBSTSubtree(self, root: TreeNode) -> int:
         def dfs(node):
             if not node:
-                return 0, 0, float('inf'), float('-inf')
+                return 0, 0, float('inf'), float('-inf')  # this line is important
             N1, n1, min1, max1 = dfs(node.left)
             N2, n2, min2, max2 = dfs(node.right)
             n = 1 + n1 + n2 if max1 < node.val < min2 else float('-inf')
-            return max(N1, N2, n), n, min(min1, node.val), max(max2, node.val)
+            return max(N1, N2, n), n, min(min1, min2, node.val), max(max1, max2, node.val)
         return dfs(root)[0]
 
 if __name__ == '__main__':
