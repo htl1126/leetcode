03691bec037f57b38383877a17066b36c964be15
htl1126 <tlhuang@tlhuang.net>
2021-05-05 23:25:13 -0400

    Modified 670.py

--- 670.py
+++ 670.py
@@ -1,20 +1,13 @@
-class Solution(object):
-    def maximumSwap(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        digit = list(str(num))
-        for i in xrange(len(digit)):
-            if digit[i] != '9':
-                max_digit = i + 1
-                for j in xrange(i + 1, len(digit)):
-                    if digit[j] >= digit[max_digit]:
-                        max_digit = j
-                if 0 <= i < len(digit) and 0 <= max_digit < len(digit) and digit[max_digit] > digit[i]:
-                    digit[i], digit[max_digit] = digit[max_digit], digit[i]
-                    break
-        return int(''.join(digit))
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        n = [int(i) for i in str(num)]
+        dic = {x: i for i, x in enumerate(n)}
+        for i, d in enumerate(n):
+            for D in range(9, d, -1):
+                if dic.get(D, -1) > i:
+                    n[i], n[dic[D]] = n[dic[D]], n[i]
+                    return int(''.join(str(i) for i in n))
+        return num
 
 if __name__ == "__main__":
     sol = Solution()
