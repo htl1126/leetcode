03c437c524a3c43f58374d96b12a5515c50fed7c
htl1126 <tlhuang@tlhuang.net>
2021-05-05 23:24:09 -0400

    Modified 5.py

--- 5.py
+++ 5.py
@@ -2,25 +2,17 @@
 #              -optimization-88ms
 
 
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        if not s:
-            return 0
-        maxlen, start = 1, 0
-        for i in xrange(1, len(s)):
-            if i - maxlen - 1 >= 0 and \
-                    s[i - maxlen - 1:i + 1] == s[i - maxlen - 1:i + 1][::-1]:
-                start = i - maxlen - 1
-                maxlen += 2
-            if i - maxlen >= 0 and \
-                    s[i - maxlen:i + 1] == s[i - maxlen:i + 1][::-1]:
-                start = i - maxlen
-                maxlen += 1
-        return s[start:start + maxlen]
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        maxlen, start = 1, 0
+        for i in range(1, len(s)):
+            if i - maxlen - 1 >= 0 and s[i - maxlen - 1:i + 1] == s[i - maxlen - 1:i + 1][::-1]:
+                start = i - maxlen - 1
+                maxlen += 2
+            if i - maxlen >= 0 and s[i - maxlen:i + 1] == s[i - maxlen:i + 1][::-1]:
+                start = i - maxlen
+                maxlen += 1
+        return s[start:start + maxlen]
 
 if __name__ == '__main__':
     sol = Solution()
