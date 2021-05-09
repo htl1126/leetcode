0d7de3d30a14e4255e57ff0c11f61cf07169b042
htl1126 <tlhuang@tlhuang.net>
2021-05-09 18:05:31 -0400

    Modified 46.py

--- 46.py
+++ 46.py
@@ -3,15 +3,15 @@
 # ref: https://discuss.leetcode.com/topic/17277/one-liners-in-python
 
 
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        return nums and [p[:i] + [nums[0]] + p[i:]
-                         for p in self.permute(nums[1:])
-                         for i in xrange(len(nums))] or [[]]
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return [[]]
+        ans = []
+        for p in self.permute(nums[1:]):
+            for i in range(len(nums)):
+                ans.append(p[:i] + [nums[0]] + p[i:])
+        return ans
 
 if __name__ == '__main__':
     sol = Solution()
