b41fd953b55eece5c57560780d1bc0ac4a6e45d4
htl1126 <tlhuang@tlhuang.net>
2021-05-10 13:52:05 -0400

    Modified 666.py

--- 666.py
+++ 666.py
@@ -1,18 +1,14 @@
+# Ref: https://leetcode.com/problems/path-sum-iv/discuss/106887/C%2B%2B-Java-Clean-Code
+
 class Solution:
-    def pathSum(self, nums: List[int]) -> int:
-        tree, d = [], 0
-        for i in range(4):
-            tree.append([[-1]] * (2 ** i))
-        for n in nums:
-            d = max(n // 100, d)
-            tree[n // 100 - 1][n % 100 // 10 - 1] = [n % 10]
-        for i in range(d - 2, -1, -1):
-            for j, n in enumerate(tree[i]):
-                if n[0] != -1:
-                    t = []
-                    for v in (tree[i + 1][2 * j] + tree[i + 1][2 * j + 1]):
-                        if v != -1:
-                            t.append(n[0] + v)
-                    if t:
-                        tree[i][j] = t
-        return sum(tree[0][0])
+    def pathSum(self, nums: List[int]) -> int:
+        tree = [[0] * 8 for _ in range(5)]
+        for n in nums:
+            d, p, v = n // 100, n % 100 // 10 - 1, n % 10
+            tree[d][p] = tree[d - 1][p // 2] + v
+        ans = 0
+        for i in range(1, 5):
+            for j in range(8):
+                if i == 4 or tree[i][j] != 0 and tree[i + 1][2 * j] == 0 and tree[i + 1][2 * j + 1] == 0:
+                    ans += tree[i][j]
+        return ans
