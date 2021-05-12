2f85286e11f2aa0344dc286f7c0521c148aaf86f
htl1126 <tlhuang@tlhuang.net>
2021-05-12 08:52:52 -0400

    Modified 426.py

--- 426.py
+++ 426.py
@@ -1,3 +1,5 @@
+# Ref: https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/discuss/169965/Python-Inorder-Transverse
+
 # Definition for a Node.
 class Node(object):
     def __init__(self, val, left=None, right=None):
@@ -13,26 +15,18 @@ class Solution(object):
         """
         if not root:
             return root
-        def convert(node):
-            if not node:
-                return None, None
-            left_head, left_tail = convert(node.left)
-            right_head, right_tail = convert(node.right)
-            node.left = node.right = None
-            new_head = new_tail = node
-            if left_head:
-                left_tail.right = node
-                node.left = left_tail
-                new_head = left_head
-            if right_head:
-                right_head.left = node
-                node.right = right_head
-                new_tail = right_tail
-            return new_head, new_tail
-        head, tail = convert(root)
-        head.left = tail
-        tail.right = head
-        return head
+        dummy = Node(0, None, None)
+        prev = dummy
+        stack, node = [], root
+        while stack or node:
+            while node:
+                stack.append(node)
+                node = node.left
+            node = stack.pop()
+            prev.right, node.left, prev = node, prev, node
+            node = node.right
+        prev.right, dummy.right.left = dummy.right, prev
+        return dummy.right
 
 if __name__ == "__main__":
     root = Node(2)
