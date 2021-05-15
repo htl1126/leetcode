5cf5174fc88ed398f7d7e6f15cf5a0dfb7b1cc9a
htl1126 <tlhuang@tlhuang.net>
2021-05-15 04:38:10 -0400

    Modified 339.py

--- 339.py
+++ 339.py
@@ -15,6 +15,23 @@ class Solution(object):
             else:
                 self.get_sum(item.getList(), depth + 1)
 
+# stack version
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        s = [[nestedList, 0, 1]]
+        ans = 0
+        while s:
+            arr, i, d = s[-1]
+            if i == len(arr):
+                s.pop()
+            else:
+                s[-1][1] += 1
+                if arr[i].isInteger():
+                    ans += arr[i].getInteger() * d
+                else:
+                    s.append([arr[i].getList(), 0, d + 1])
+        return ans
+
 if __name__ == '__main__':
     sol = Solution()
     print sol.depthSum([1, [4, [6]]])
