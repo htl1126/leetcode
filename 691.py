ff16110ec171f17bfd25a8a15b9fdf1ddc003f6e
htl1126 <tlhuang@tlhuang.net>
2021-05-19 22:47:36 -0400

    Modified 691.py

--- 691.py
+++ 691.py
@@ -1,18 +1,24 @@
-# Ref: https://leetcode.com/problems/stickers-to-spell-word/discuss/108333/Rewrite-of-contest-winner's-solution
+# Ref: https://leetcode.com/problems/stickers-to-spell-word/discuss/108318/C%2B%2BJavaPython-DP-%2B-Memoization-with-optimization-29-ms-(C%2B%2B)
 
-class Solution:
-    def minStickers(self, stickers: List[str], target: str) -> int:
-        n = 1 << len(target)
-        dp = [float('inf')] * n
-        dp[0] = 0
-        for i in range(n):
-            if dp[i] != float('inf'):
-                for sticker in stickers:
-                    now = i
-                    for s in sticker:
-                        for j, c in enumerate(target):
-                            if s == c and not ((now >> j) & 1):
-                                now |= 1 << j
-                                break
-                    dp[now] = min(dp[now], dp[i] + 1)
-        return dp[-1] if dp[-1] < float('inf') else -1
+class Solution(object):
+    def minStickers(self, stickers, target):
+        num_sticker = len(stickers)
+        s_cnt = [collections.Counter(s) for s in stickers]    
+        memo = {}
+        memo[""] = 0
+        
+        def helper(target):
+            if target not in memo:
+                t_cnt = collections.Counter(target)
+                ans = float('inf')
+                for i in range(num_sticker):
+                    if s_cnt[i][target[0]] == 0:
+                        continue
+                    s = "".join([c * (n - s_cnt[i][c]) for c, n in t_cnt.items() if n > s_cnt[i][c]])
+                    tmp = helper(s)
+                    if tmp != -1: 
+                        ans = min(ans, 1 + tmp)
+                memo[target] = ans if ans < float('inf') else -1
+            return memo[target]
+        
+        return helper("".join(sorted(target)))
