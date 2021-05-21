b00026ac89942a142e7bcee6e5ca4c183443031b
htl1126 <tlhuang@tlhuang.net>
2021-05-21 16:13:10 -0400

    Modified 140.py

--- 140.py
+++ 140.py
@@ -1,21 +1,21 @@
 # ref: https://discuss.leetcode.com/topic/35762/9-lines-python-10-lines-c
 
 
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: Set[str]
-        :rtype: List[str]
-        """
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         memo = {len(s): ['']}
 
         def sentences(i):
             if i not in memo:
-                memo[i] = [s[i:j] + (tail and ' ' + tail)
-                           for j in xrange(i + 1, len(s) + 1)
-                           if s[i:j] in wordDict
-                           for tail in sentences(j)]
+                t = []
+                for j in range(i + 1, len(s) + 1):
+                    if s[i:j] in wordDict:
+                        for tail in sentences(j):
+                            if tail:
+                                t.append(s[i:j] + ' ' + tail)
+                            else:
+                                t.append(s[i:j])
+                memo[i] = t
             return memo[i]
         return sentences(0)
 
