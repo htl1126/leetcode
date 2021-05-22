e84c46e5a7fd66fd9f89b65d3f1eab92454f1369
htl1126 <tlhuang@tlhuang.net>
2021-05-21 21:40:04 -0400

    Modified 480.py

--- 480.py
+++ 480.py
@@ -1,21 +1,37 @@
 # Ref: https://leetcode.com/problems/sliding-window-median/discuss/96355/Easy-Python-O(nk)
+# Ref: https://leetcode.com/problems/sliding-window-median/discuss/262689/Python-Small-and-Large-Heaps
 
-import bisect
 
-class Solution(object):
-    def medianSlidingWindow(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[float]
-        """
-        window = sorted(nums[:k])
-        medians = []
-        for a, b in zip(nums, nums[k:] + [0]):
-            medians.append((window[k / 2] + window[~(k / 2)]) / 2.0)
-            window.remove(a)
-            bisect.insort(window, b)
-        return medians
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        def move(h1, h2):
+            x, i = heapq.heappop(h1)
+            heapq.heappush(h2, (-x, i))
+	
+        def get_med(h1, h2, k):
+            return float(h2[0][0]) if k & 1 else (h2[0][0] - h1[0][0]) / 2
+
+        small, large = [], []
+        for i, x in enumerate(nums[:k]): 
+            heapq.heappush(small, (-x, i))
+        for _ in range(k - (k >> 1)): 
+            move(small, large)
+        ans = [get_med(small, large, k)]
+        for i, x in enumerate(nums[k:]):
+            if x >= large[0][0]:
+                heapq.heappush(large, (x, i + k))
+                if nums[i] <= large[0][0]: 
+                    move(large, small)
+            else:
+                heapq.heappush(small, (-x, i + k))
+                if nums[i] >= large[0][0]: 
+                    move(small, large)
+            while small and small[0][1] <= i: 
+                heapq.heappop(small)
+            while large and large[0][1] <= i: 
+                heapq.heappop(large)
+            ans.append(get_med(small, large, k))
+        return ans
 
 if __name__ == "__main__":
     sol = Solution()
