5a47df938b34a0353fc57e5a76d3b7c3578d00a9
htl1126 <tlhuang@tlhuang.net>
2021-05-22 23:03:36 -0400

    Modified 282.py

--- 282.py
+++ 282.py
@@ -4,7 +4,7 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         res = []
-        def dfs(num, temp, cur, last, res):
+        def dfs(num, temp, cur, last):
             if not num:
                 if cur == target:
                     res.append(temp)
@@ -12,12 +12,12 @@ class Solution:
             for i in range(1, len(num) + 1):
                 val = num[:i]
                 if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
-                    dfs(num[i:], temp + "+" + val, cur + int(val), int(val), res)
-                    dfs(num[i:], temp + "-" + val, cur - int(val), - int(val), res)
-                    dfs(num[i:], temp + "*" + val, cur - last + last * int(val), last*int(val), res)
+                    dfs(num[i:], temp + "+" + val, cur + int(val), int(val))
+                    dfs(num[i:], temp + "-" + val, cur - int(val), - int(val))
+                    dfs(num[i:], temp + "*" + val, cur - last + last * int(val), last * int(val))
         for i in range(1, len(num) + 1):
             if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
-                dfs(num[i:], num[:i], int(num[:i]), int(num[:i]), res)
+                dfs(num[i:], num[:i], int(num[:i]), int(num[:i]))
         return res
 
 if __name__ == '__main__':
