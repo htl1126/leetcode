f0f574b21045738c8d9090d54249e981ed059750
htl1126 <tlhuang@tlhuang.net>
2021-05-22 22:55:41 -0400

    Modified 282.py

--- 282.py
+++ 282.py
@@ -1,33 +1,24 @@
 # ref: https://discuss.leetcode.com/topic/30089/clean-python-dfs-with-comments
 
 
-class Solution(object):
-    def addOperators(self, num, target):
-        """
-        :type num: str
-        :type target: int
-        :rtype: List[str]
-        """
-        res, self.target = [], target
+class Solution:
+    def addOperators(self, num: str, target: int) -> List[str]:
+        res = []
+        def dfs(num, temp, cur, last, res):
+            if not num:
+                if cur == target:
+                    res.append(temp)
+                return
+            for i in range(1, len(num) + 1):
+                val = num[:i]
+                if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
+                    dfs(num[i:], temp + "+" + val, cur + int(val), int(val), res)
+                    dfs(num[i:], temp + "-" + val, cur - int(val), - int(val), res)
+                    dfs(num[i:], temp + "*" + val, cur - last + last * int(val), last*int(val), res)
         for i in range(1, len(num) + 1):
             if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
-                self.dfs(num[i:], num[:i], int(num[:i]), int(num[:i]), res)
+                dfs(num[i:], num[:i], int(num[:i]), int(num[:i]), res)
         return res
-
-    def dfs(self, num, temp, cur, last, res):
-        if not num:
-            if cur == self.target:
-                res.append(temp)
-            return
-        for i in range(1, len(num) + 1):
-            val = num[:i]
-            if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
-                self.dfs(num[i:], temp + "+" + val, cur + int(val),
-                         int(val), res)
-                self.dfs(num[i:], temp + "-" + val, cur - int(val),
-                         - int(val), res)
-                self.dfs(num[i:], temp + "*" + val,
-                         cur - last + last * int(val), last*int(val), res)
 
 if __name__ == '__main__':
     sol = Solution()
