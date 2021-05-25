8c8c8952c6cccc28a2f29a0445730acaaf414ec9
htl1126 <tlhuang@tlhuang.net>
2021-05-25 00:58:04 -0400

    Modified 166.py

--- 166.py
+++ 166.py
@@ -2,25 +2,19 @@
 #              python-solution-using-dictionary
 
 
-class Solution(object):
-    def fractionToDecimal(self, numerator, denominator):
-        """
-        :type numerator: int
-        :type denominator: int
-        :rtype: str
-        """
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
         sign = '-' if numerator * denominator < 0 else ''
         head, remainder = divmod(abs(numerator), abs(denominator))
         tail, seen = '', {}
         while remainder != 0:
             if remainder in seen:
-                tail = tail[:seen[remainder]] + '(' +\
-                    tail[seen[remainder]:] + ')'
+                tail = tail[:seen[remainder]] + '(' + tail[seen[remainder]:] + ')'
                 break
             seen[remainder] = len(tail)
             digit, remainder = divmod(remainder * 10, abs(denominator))
-            tail = '{0}{1}'.format(tail, digit)
-        return '{0}{1}{2}'.format(sign, head, (tail and '.' + tail))
+            tail += str(digit)
+        return sign + str(head) + ('.' + tail if tail else '')
 
 if __name__ == '__main__':
     sol = Solution()
