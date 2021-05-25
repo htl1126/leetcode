56ca7c8c1f917b1509327ebcd2f3f3de5da5701c
htl1126 <tlhuang@tlhuang.net>
2021-05-25 00:52:06 -0400

    Modified 29.py

--- 29.py
+++ 29.py
@@ -6,22 +6,22 @@ import sys
 
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == 0:
-            return 0
-        is_neg = (dividend > 0) ^ (divisor > 0)
-        dividend, divisor = abs(dividend), abs(divisor)
-        ans, t, q = 0, divisor, 1
-        while t <= dividend:
-            t <<= 1
-            q <<= 1
-        while t >= divisor:
-            if dividend >= t:
-                dividend -= t
-                ans += q
-            t >>= 1
-            q >>= 1
-        ans = -ans if is_neg else ans
-        return min(2 ** 31 - 1, max(ans, -2 ** 31))
+        if dividend == 0:
+            return 0
+        is_neg = dividend * divisor < 0
+        dividend, divisor = abs(dividend), abs(divisor)
+        ans, t, q = 0, divisor, 1
+        while t <= dividend:
+            t <<= 1
+            q <<= 1
+        while t >= divisor:
+            if dividend >= t:
+                dividend -= t
+                ans += q
+            t >>= 1
+            q >>= 1
+        ans = -ans if is_neg else ans
+        return min(2 ** 31 - 1, max(ans, -2 ** 31))
 
 if __name__ == '__main__':
     sol = Solution()
