fd376d82c4f3298a6ce0d7e3159f3492f269cd53
htl1126 <tlhuang@tlhuang.net>
2021-05-25 23:45:04 -0400

    Modified 76.py

--- 76.py
+++ 76.py
@@ -9,16 +9,19 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
+        # missing: the number of chars needed to for taking all the chars in t
+        # need: freq of each char in the window; negative: we are having more than we need
+        #                                        positive: we still need more
         need, missing = collections.Counter(t), len(t)
         i = I = J = 0
         for j, c in enumerate(s, 1):
             missing -= need[c] > 0
             need[c] -= 1
-            if not missing:
-                while i < j and need[s[i]] < 0:
+            if not missing:  # we have collected all the chars in t
+                while i < j and need[s[i]] < 0:  # shrink the window
                     need[s[i]] += 1
                     i += 1
-                if not J or j - i < J - I:
+                if not J or j - i < J - I:  # I and J are 0's in the beginning
                     I, J = i, j
         return s[I:J]
 
