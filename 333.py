f2c6e1d62677fdeace8cb80451f2d0bcb493305c
htl1126 <tlhuang@tlhuang.net>
2021-05-26 23:14:32 -0400

    Modified 333.py

--- 333.py
+++ 333.py
@@ -17,7 +17,7 @@ class Solution:
             N1, n1, min1, max1 = dfs(node.left)
             N2, n2, min2, max2 = dfs(node.right)
             n = 1 + n1 + n2 if max1 < node.val < min2 else float('-inf')
-            return max(N1, N2, n), n, min(min1, min2, node.val), max(max1, max2, node.val)
+            return max(N1, N2, n), n, min(min1, node.val), max(max2, node.val)
         return dfs(root)[0]
 
 if __name__ == '__main__':
