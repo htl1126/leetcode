62c81cad44b032d5321c1f329ce41f3d40e1a278
htl1126 <tlhuang@tlhuang.net>
2021-05-27 18:31:49 -0400

    Modified 525.py

--- 525.py
+++ 525.py
@@ -1,16 +1,12 @@
 # Ref: https://leetcode.com/problems/contiguous-array/discuss/99655/Python-O(n)-Solution-with-Visual-Explanation
 
-class Solution(object):
-    def findMaxLength(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        max_len = count = 0
-        d = {0: 0}
-        for i, num in enumerate(nums, 1):
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        max_len = count = 0 
+        d = {0: -1}
+        for i, num in enumerate(nums):
             count += 1 if num else -1
-            max_len = max(max_len, i - d.setdefault(count, i))
+            max_len = max(max_len, i - d.setdefault(count, i)) 
         return max_len
 
 if __name__ == "__main__":
