288713af7cc8e2be6bd11b9dc8e3c84af7fd9d59
htl1126 <tlhuang@tlhuang.net>
2021-05-27 10:27:10 -0400

    Modified 139.py

--- 139.py
+++ 139.py
@@ -2,21 +2,16 @@
 #              solution-in-python-with-description
 
 
-class Solution(object):
-    def wordBreak(self, s, wordDict):
-        """
-        :type s: str
-        :type wordDict: Set[str]
-        :rtype: bool
-        """
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         if len(s) == 0:
             return True
-        dp = [False for _ in xrange(len(s))]
-        for i in xrange(len(s)):
+        dp = [False] * len(s)
+        for i in range(len(s)):
             for w in wordDict:
-                if w == s[i - len(w) + 1: i + 1] and (dp[i - len(w)]
-                                                      or i - len(w) == -1):
-                    dp[i] = True
+                if w == s[i - len(w) + 1: i + 1]:
+                    if i - len(w) >= 0 and dp[i - len(w)] or i -len(w) + 1 == 0:
+                        dp[i] = True
         return dp[-1]
 
 if __name__ == '__main__':
