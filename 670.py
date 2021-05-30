a53a5658f0d97eb42c8018c1ac8bc277a955d81e
htl1126 <tlhuang@tlhuang.net>
2021-05-30 17:19:37 -0400

    Modified 670.py

--- 670.py
+++ 670.py
@@ -1,13 +1,14 @@
 class Solution:
-    def maximumSwap(self, num: int) -> int:
-        n = [int(i) for i in str(num)]
-        dic = {x: i for i, x in enumerate(n)}
-        for i, d in enumerate(n):
-            for D in range(9, d, -1):
-                if dic.get(D, -1) > i:
-                    n[i], n[dic[D]] = n[dic[D]], n[i]
-                    return int(''.join(str(i) for i in n))
-        return num
+    def maximumSwap(self, num: int) -> int:
+        n = [int(i) for i in str(num)]
+        dic = {x: i for i, x in enumerate(n)}
+        # for d = n[i] (i = 0, 1, 2, ...), find the largest D = n[j], j > i such that D > d
+        for i, d in enumerate(n):
+            for D in range(9, d, -1):
+                if dic.get(D, -1) > i:
+                    n[i], n[dic[D]] = n[dic[D]], n[i]
+                    return int(''.join(str(i) for i in n))
+        return num
 
 if __name__ == "__main__":
     sol = Solution()
