005edae48c5b2b3c7dc2073a4bd788669bf998ef
htl1126 <tlhuang@tlhuang.net>
2021-06-01 17:12:20 -0400

    Modified 113.py

--- 113.py
+++ 113.py
@@ -5,23 +5,18 @@ class TreeNode(object):
         self.left = None
         self.right = None
 
-class Solution(object):
-    def pathSum(self, root, sum):
-        """
-        :type root: TreeNode
-        :type sum: int
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
         result = []
-        def find_path(node, path, current_sum, sum, result):
-            if current_sum == sum and not node.left and not node.right:
+        def find_path(node, path, current_sum, target):
+            if current_sum == target and not node.left and not node.right:
                 result.append(path)
             if node.left:
-                find_path(node.left, path + [node.left.val], current_sum + node.left.val, sum, result)
+                find_path(node.left, path + [node.left.val], current_sum + node.left.val, target)
             if node.right:
-                find_path(node.right, path + [node.right.val], current_sum + node.right.val, sum, result)
+                find_path(node.right, path + [node.right.val], current_sum + node.right.val, target)
         if root:
-            find_path(root, [root.val], root.val, sum, result)
+            find_path(root, [root.val], root.val, targetSum)
         return result
 
 if __name__ == '__main__':
