5394b77da62a933f59d5a4438f08db721da415b2
htl1126 <tlhuang@tlhuang.net>
2021-06-02 16:00:51 -0400

    Modified 282.py

--- 282.py
+++ 282.py
@@ -4,17 +4,17 @@
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         res = []
-        def dfs(num, temp, cur, last):
-            if not num:
-                if cur == target:
-                    res.append(temp)
+        def dfs(rest_s, cur_s, cur_sum, last_term):
+            if not rest_s:
+                if cur_sum == target:
+                    res.append(cur_s)
                 return
-            for i in range(1, len(num) + 1):
-                val = num[:i]
-                if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
-                    dfs(num[i:], temp + "+" + val, cur + int(val), int(val))
-                    dfs(num[i:], temp + "-" + val, cur - int(val), - int(val))
-                    dfs(num[i:], temp + "*" + val, cur - last + last * int(val), last * int(val))
+            for i in range(1, len(rest_s) + 1):
+                val = rest_s[:i]
+                if i == 1 or (i > 1 and rest_s[0] != "0"):  # prevent "00*" as a number
+                    dfs(rest_s[i:], cur_s + "+" + val, cur_sum + int(val), int(val))
+                    dfs(rest_s[i:], cur_s + "-" + val, cur_sum - int(val), - int(val))
+                    dfs(rest_s[i:], cur_s + "*" + val, cur_sum - last_term + last_term * int(val), last_term * int(val))
         for i in range(1, len(num) + 1):
             if i == 1 or (i > 1 and num[0] != "0"):  # prevent "00*" as a number
                 dfs(num[i:], num[:i], int(num[:i]), int(num[:i]))
