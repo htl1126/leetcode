beaaefb82f2ea54aa45c820f912ffce7257f8291
htl1126 <tlhuang@tlhuang.net>
2021-06-11 20:10:40 -0400

    Modified 1263.py

--- 1263.py
+++ 1263.py
@@ -1,33 +1,44 @@
-# Ref: https://leetcode.com/problems/minimum-moves-to-move-a-box-to-their-target-location/discuss/431528/Python-Dijkstra-Short
+# Ref: https://leetcode.com/problems/minimum-moves-to-move-a-box-to-their-target-location/discuss/693918/Python-BFS-*-BFS-or-130ms-or-beats-95-or-Explained-and-Commented
 
 class Solution:
-    def minPushBox(self, grid: List[List[str]]) -> int:
-        player, box, target, empty_pos = None, None, None, set()
-        for i, r in enumerate(grid):
-            for j, c in enumerate(r):
-                if c == '#':
-                    continue
-                elif c == 'S':
-                    player = i, j
-                elif c == 'B':
-                    box = i, j
-                elif c == 'T':
-                    target = i, j
-                empty_pos.add((i, j))
+    def minPushBox(self, grid: List[List[str]]) -> int:
+        player, box, target, non_obstacle = None, None, None, set()
+        row, col = len(grid), len(grid[0])
+        for i, r in enumerate(grid):
+            for j, c in enumerate(r):
+                if c == '#':
+                    continue
+                elif c == 'S':
+                    player = i, j
+                elif c == 'B':
+                    box = i, j
+                elif c == 'T':
+                    target = i, j
+                non_obstacle.add((i, j))
 
-        h, seen = [(0, *player, *box)], set()
-        while h:
-            m, pi, pj, bi, bj = heapq.heappop(h)
-            if (bi, bj) == target: 
-                return m
-            if (pi, pj, bi, bj) in seen:
-                continue
-            seen.add((pi, pj, bi, bj))
-            for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-                new_pi, new_pj, new_bi, new_bj = pi + dx, pj + dy, bi + dx, bj + dy
-                if (new_pi, new_pj) in empty_pos:
-                    if (new_pi, new_pj) == (bi, bj):
-                        heapq.heappush(h, (m + 1, new_pi, new_pj, new_bi, new_bj))
-                    else:
-                        heapq.heappush(h, (m, new_pi, new_pj, bi, bj))
-        return -1
+        def check(cur, dest, box):
+            q = collections.deque([cur])
+            seen = set(cur)
+            while q:
+                i, j = q.popleft()
+                if (i, j) == dest:
+                    return True
+                for x, y in (i + 1, j), (i - 1, j), (i, j - 1), (i, j + 1):
+                    if (x, y) in non_obstacle and (x, y) not in seen and (x, y) != box:
+                        seen.add((x, y))
+                        q.append((x, y))
+            return False
+
+        q, seen = collections.deque([(0, *player, *box)]), set(player + box)
+        while q:
+            m, pi, pj, bi, bj = q.popleft()
+            if (bi, bj) == target:
+                return m
+            new_box = [(bi + 1, bj), (bi - 1, bj), (bi, bj + 1), (bi, bj - 1)]
+            new_player = [(bi - 1, bj), (bi + 1, bj), (bi, bj - 1), (bi, bj + 1)]
+            for new_b, new_p in zip(new_box, new_player):
+                if new_b in non_obstacle and (bi, bj) + new_b not in seen:
+                    if new_p in non_obstacle and check((pi, pj), new_p, (bi, bj)):
+                        seen.add((bi, bj) + new_b)
+                        q.append((m + 1, bi, bj, new_b[0], new_b[1]))
+        return -1
