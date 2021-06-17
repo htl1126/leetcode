8b12a965a994bc6154e27c145ca98bb5d515f8eb
htl1126 <tlhuang@tlhuang.net>
2021-06-17 13:20:56 -0400

    Modified 54.py

--- 54.py
+++ 54.py
@@ -1,31 +1,14 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: List[int]
-        """
-        if matrix == []:
-            return []
-        row, col = len(matrix), len(matrix[0])
-        displace = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        direct_now = 0
-        x, y = 0, -1
-        result = [0 for _ in xrange(row * col)]
-        length = row * col
-        pos = 0
-        while 1:
-            displace_x, displace_y = displace[direct_now]
-            if 0 <= x + displace_x < row and 0 <= y + displace_y < col and \
-                    matrix[x + displace_x][y + displace_y] != None:
-                x += displace_x
-                y += displace_y
-                result[pos] = matrix[x][y]
-                matrix[x][y] = None
-                pos += 1
-                if pos == length:
-                    return result
-            else:
-                direct_now = (direct_now + 1) % 4
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        x, y, dx, dy, ans = 0, 0, 0, 1, []
+        m, n = len(matrix), len(matrix[0])
+        for _ in range(m * n):
+            ans.append(matrix[x][y])
+            matrix[x][y] = 101  # -100 <= matrix[x][y] <= 100
+            if not(0 <= x + dx < m and 0 <= y + dy < n and matrix[x + dx][y + dy] != 101):
+                dx, dy = dy, -dx
+            x, y = x + dx, y + dy
+        return ans
 
 if __name__ == '__main__':
     sol = Solution()
