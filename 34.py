459d7049156a76aa3330306c9bd0ded3e5516bfb
htl1126 <tlhuang@tlhuang.net>
2021-06-17 08:02:31 -0400

    Modified 34.py

--- 34.py
+++ 34.py
@@ -1,18 +1,18 @@
 # Ref: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/discuss/14707/9-11-lines-O(log-n)
 
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def search(n):
-            l, r = 0, len(nums)  # notice the r value
-            while l < r:
-                m = (l + r) // 2
-                if nums[m] < n:
-                    l = m + 1
-                else:
-                    r = m
-            return l
-        left = search(target)
-        return [left, search(target + 1) - 1] if target in nums[left:left + 1] else [-1, -1]
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def search(n):
+            l, r = 0, len(nums)  # notice the r value
+            while l < r:
+                m = (l + r) // 2
+                if nums[m] < n:
+                    l = m + 1
+                else:
+                    r = m
+            return l
+        left = search(target)
+        return [left, search(target + 1) - 1] if target in nums[left:left + 1] else [-1, -1]
 
 if __name__ == '__main__':
     sol = Solution()
