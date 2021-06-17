514476c71827b26dbe79997cbfd5f5fe39d8051f
htl1126 <tlhuang@tlhuang.net>
2021-06-17 17:00:50 -0400

    Modified 43.py

--- 43.py
+++ 43.py
@@ -1,24 +1,19 @@
 import sys
 
 
-class Solution(object):
-    def multiply(self, num1, num2):
-        """
-        :type num1: str
-        :type num2: str
-        :rtype: str
-        """
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
         num1 = [int(i) for i in num1][::-1]
         num2 = [int(i) for i in num2][::-1]
         len1, len2 = len(num1), len(num2)
-        ans = [0 for _ in xrange(len1 + len2)]
-        for i in xrange(len1):
-            for j in xrange(len2):
+        ans = [0 for _ in range(len1 + len2)]
+        for i in range(len1):
+            for j in range(len2):
                 ans[i + j] += num1[i] * num2[j]
-        for i in xrange(len(ans) - 1):
-            ans[i + 1] += ans[i] / 10
+        for i in range(len(ans) - 1):
+            ans[i + 1] += ans[i] // 10
             ans[i] %= 10
-        for i in xrange(len(ans) - 1, -1, -1):
+        for i in range(len(ans) - 1, -1, -1):
             if ans[i] != 0:
                 return ''.join([str(i) for i in reversed(ans[:i + 1])])
         return '0'
