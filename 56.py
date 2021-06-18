2b3df8b08162adb8e9792a614c01edd4a7d8dc7e
htl1126 <tlhuang@tlhuang.net>
2021-06-18 13:01:23 -0400

    Modified 56.py

--- 56.py
+++ 56.py
@@ -1,28 +1,16 @@
 # ref: https://leetcode.com/discuss/42344/7-lines-easy-python
 
 
-# Definition for an interval.
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-
-
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
-        """
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         result = []
-        for i in sorted(intervals, key=lambda i: i.start):
-            if result and result[-1].end >= i.start:
-                result[-1].end = max(result[-1].end, i.end)
+        for i in sorted(intervals, key=lambda i: i[0]):
+            if result and result[-1][1] >= i[0]:
+                result[-1][1] = max(result[-1][1], i[1])
             else:
-                result += i,
+                result.append(i)
         return result
 
 if __name__ == '__main__':
     sol = Solution()
-    print sol.merge([Interval(1, 3), Interval(2, 6), Interval(8, 10),
-                     Interval(15, 18)])
+    print sol.merge([[1, 3], [2, 6], [8, 10], [15, 18]])
