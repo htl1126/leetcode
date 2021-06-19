d953ebb39c9f3c05232058f69d986a5dda963009
htl1126 <tlhuang@tlhuang.net>
2021-06-19 12:37:02 -0400

    Modified 273.py

--- 273.py
+++ 273.py
@@ -1,12 +1,8 @@
 # ref: https://leetcode.com/discuss/55477/recursive-python
 
 
-class Solution(object):
-    def numberToWords(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
+class Solution:
+    def numberToWords(self, num: int) -> str:
         to19 = ('One Two Three Four Five Six Seven Eight Nine Ten '
                 'Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen '
                 'Eighteen Nineteen').split(' ')
@@ -15,14 +11,14 @@ class Solution(object):
 
         def words(n):
             if n < 20:
-                return to19[n - 1:n]
+                return to19[n - 1:n]  # return "" for zero
             if n < 100:
-                return [tens[n / 10 - 2]] + words(n % 10)
+                return [tens[n // 10 - 2]] + words(n % 10)
             if n < 1000:
-                return [to19[n / 100 - 1]] + ['Hundred'] + words(n % 100)
+                return [to19[n // 100 - 1]] + ['Hundred'] + words(n % 100)
             for p, w in enumerate(('Thousand', 'Million', 'Billion'), 1):
                 if n < 1000 ** (p + 1):
-                    return words(n / 1000 ** p) + [w] + words(n % 1000 ** p)
+                    return words(n // 1000 ** p) + [w] + words(n % 1000 ** p)
         return ' '.join(words(num)) or 'Zero'
 
 if __name__ == '':
