03ca579ddbb43d098f640eb79a1edf4c5d4c4469
htl1126 <tlhuang@tlhuang.net>
2021-06-20 13:24:23 -0400

    Modified 75.py

--- 75.py
+++ 75.py
@@ -1,29 +1,26 @@
-# source: https://leetcode.com/discuss/84177/python-o-n-1-pass
-#                 -in-place-solution-with-explanation
-
-import sys
-from leetcode_util import read_list
+# ref: https://leetcode.com/problems/sort-colors/discuss/26481/Python-O(n)-1-pass-in-place-solution-with-explanation
 
 
-class Solution(object):
-    def sortColors(self, nums):
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        zero = 0
-        second = len(nums) - 1
-        i = 0
-        while i <= second:
-            # Need to sort 2 first then 0: case [1, 2, 0]
-            while nums[i] == 2 and i < second:
-                nums[i], nums[second] = nums[second], nums[i]
-                second -= 1
-            while nums[i] == 0 and i > zero:
-                nums[i], nums[zero] = nums[zero], nums[i]
+        zero, cur, two = 0, 0, len(nums) - 1
+        # "zero" is always less than or equal to "cur",
+        # and whenever we exchange nums[cur] and nums[two], cur won't change.
+        # We don't change "cur" when nums[cur] == 2 and will process it
+        # in the next iteration (by checking its value is 0 or 1)
+        while cur <= two:
+            if nums[cur] == 0:
+                nums[zero], nums[cur] = nums[cur], nums[zero]
                 zero += 1
-            i += 1
-        return nums
+                cur += 1
+            elif nums[cur] == 1:
+                cur += 1
+            elif nums[cur] == 2:
+                nums[cur], nums[two] = nums[two], nums[cur]
+                two -= 1
 
 
 if __name__ == '__main__':
