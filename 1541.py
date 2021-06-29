246130b5b96ed8fdf39fffd93d26f197667eadfe
htl1126 <tlhuang@tlhuang.net>
2021-06-29 15:27:34 -0400

    Modified 1541.py

--- 1541.py
+++ 1541.py
@@ -2,19 +2,19 @@
 
 class Solution:
     def minInsertions(self, s: str) -> int:
-        # added: actually added right parentheses
-        # right_need: additionally needed right parentheses
+        # added: the parentheses added in the middle of the input string
+        # right_need: the parentheses appended to the end of the input string
         added = right_need = 0
         for c in s:
             if c == '(':
-                right_need += 2
-                if right_need % 2:
-                    added += 1
-                    right_need -= 1
+                right_need += 2      # we append two ")" when we see "("
+                if right_need % 2:   # if we have odd number of ")" to be appended,
+                    added += 1       # we have to move the lonely ")" in the middle
+                    right_need -= 1  # and "right_needed" need to decreased by one
             elif c == ')':
-                if right_need == 0:
-                    added += 1
-                    right_need += 1
+                if right_need == 0:  # when we don't have ")" to be appended,
+                    added += 1       # we put a "(" in the middle,
+                    right_need += 1  # and need to append a ")" to the end
                 else:
                     right_need -= 1
         return added + right_need
