f0ec53f911e5cbb6b331f495220b20c6e301cb84
htl1126 <tlhuang@tlhuang.net>
2021-07-02 16:34:21 -0400

    Modified 47.py

--- 47.py
+++ 47.py
@@ -2,19 +2,26 @@
 #              line-to-handle-duplication-beat-99-of-others
 
 
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        # The process:
+        #   Example input: [1, 1, 2]
+        #
+        #   n = 1
+        #   => ans = [[1]]
+        #
+        #   n = 1
+        #   => ans = [[1, 1]], note here we should avoid duplicates
+        #
+        #   n = 2
+        #   => ans = [[2, 1, 1], [1, 2, 1], [1, 1, 2]]
         ans = [[]]
         for n in nums:
             new_ans = []
             for l in ans:
                 for i in xrange(len(l) + 1):
                     new_ans.append(l[:i] + [n] + l[i:])
-                    if i < len(l) and l[i] == n:
+                    if i < len(l) and l[i] == n:  # avoid duplicates
                         break
             ans = new_ans
         return ans
