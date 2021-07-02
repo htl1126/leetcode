b1d8ddf58d89a62820cc6bfd25b3a35fb9cf0172
htl1126 <tlhuang@tlhuang.net>
2021-07-02 16:18:09 -0400

    Modified 371.py

--- 371.py
+++ 371.py
@@ -1,23 +1,19 @@
-# ref: https://discuss.leetcode.com/topic/49771/java-simple-easy-understand
-#              -solution-with-explanation
-# ref: https://discuss.leetcode.com/topic/49900/python-solution/2
+# ref: https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary%3A-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
+# ref: https://leetcode.com/problems/sum-of-two-integers/discuss/84282/Python-solution-with-no-%22%2B-*%22-completely-bit-manipulation-guaranteed
 
 
-class Solution(object):
-    def getSum(self, a, b):
-        """
-        :type a: int
-        :type b: int
-        :rtype: int
-        """
-        MAX_INT = 0x7FFFFFFF
-        MIN_INT = 0x80000000
-        mask = 0x100000000
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        MAX_INT = 0x7FFFFFFF  # 2 ^ 31 - 1
+        mask = 0xFFFFFFFF     # 2 ^ 32 - 1
         while b != 0:
-            carry = a & b
-            a = (a ^ b) % mask
-            b = (carry << 1) % mask
-        return a if a <= MAX_INT else ~((a % MIN_INT) ^ MAX_INT)
+            # "a ^ b" does the addition without carries
+            # "a & b" gets the carries for each bit
+            # "(a & b) << 1" moves carry bits one shift to the left.
+            # They would be added for the next iteration
+            a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+        # if a is negative, we have to flip all the leading zeros to ones
+        return a if a <= MAX_INT else ~(a ^ mask)
 
 if __name__ == '__main__':
     sol = Solution()
