223754b75e72375ecf8bdb26324837f88b92420f
htl1126 <tlhuang@tlhuang.net>
2021-07-05 18:21:24 -0400

    Modified 22.py

--- 22.py
+++ 22.py
@@ -4,19 +4,15 @@
 import sys
 from leetcode_util import read_list
 
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
         table = []
-        for i in xrange(n + 1):
+        for i in range(n + 1):
             table.append([])
         table[0].append('')
-        for i in xrange(n + 1):
-            for j in xrange(i):
-                table[i] += ['({0}){1}'.format(x, y) for x in table[j] for y in table[i - j - 1]]
+        for i in range(n + 1):
+            for j in range(i):
+                table[i] += ['(' + x + ')' + y for x in table[j] for y in table[i - j - 1]]
         return table[n]
 
 if __name__ == '__main__':
