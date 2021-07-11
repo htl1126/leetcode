4c77123308a8fc9cf7cb0b215aa9ba97288acf09
htl1126 <tlhuang@tlhuang.net>
2021-07-11 14:15:34 -0400

    Modified 17.py

--- 17.py
+++ 17.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        """
-        :type digits: str
-        :rtype: List[str]
-        """
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
         dic = {'0': ' ', '1': '*', '2': 'abc', '3': 'def', '4': 'ghi',
                '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-        ans = []
-        if digits == '':
+        if not digits:
             return []
-
-        def get_comb(digits, res):
-            if digits == '':
-                ans.append(res)
-            else:
-                for c in dic[digits[0]]:
-                    get_comb(digits[1:], res + c)
-        get_comb(digits, '')
+        if len(digits) == 1:
+            return list(dic[digits])
+        ans = []
+        for s in self.letterCombinations(digits[1:]):
+            for c in dic[digits[0]]:
+                ans.append(c + s)
         return ans
 
 if __name__ == '__main__':
