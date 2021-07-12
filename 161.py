7e1d21d6d11cfe4bf3ade4a502ea6a2cb2551c13
htl1126 <tlhuang@tlhuang.net>
2021-07-12 18:01:56 -0400

    Modified 161.py

--- 161.py
+++ 161.py
@@ -1,18 +1,13 @@
 # ref: https://leetcode.com/discuss/54608/python-concise-solution-with-comments
 
 
-class Solution(object):
-    def isOneEditDistance(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isOneEditDistance(self, s: str, t: str) -> bool:
         if len(s) > len(t):
-            return self.isOneEditDistance(t, s)
+            s, t = t, s
         if abs(len(s) - len(t)) > 1 or s == t:
             return False
-        for i in xrange(len(s)):
+        for i in range(len(s)):
             if s[i] != t[i]:
                 return s[i + 1:] == t[i + 1:] or s[i:] == t[i + 1:]
         return True
