b510e78f4d1b117554fac4bde89122d7a20103f5
htl1126 <tlhuang@tlhuang.net>
2021-07-14 10:08:44 -0400

    Modified 678.py

--- 678.py
+++ 678.py
@@ -1,26 +1,25 @@
 # Ref: https://leetcode.com/problems/valid-parenthesis-string/discuss/107570/JavaC%2B%2BPython-One-Pass-Count-the-Open-Parenthesis
 # Ref: https://leetcode.com/problems/valid-parenthesis-string/discuss/543521/Java-Count-Open-Parenthesis-O(n)-time-O(1)-space-Picture-Explain
+# Ref: https://leetcode.com/problems/valid-parenthesis-string/discuss/107577/Short-Java-O(n)-time-O(1)-space-one-pass
 
-class Solution(object):
-    def checkValidString(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def checkValidString(self, s: str) -> bool:
         cmin = cmax = 0
+        # We keep an interval of ')' needed with [cmin, cmax]
+        # So when cmin is less than 0, we 'eliminate' the 'route'
         for c in s:
             if c == '(':
-                cmin += 1
                 cmax += 1
-            if c == ')':
-                cmax -= 1
-                cmin = max(cmin - 1, 0)
+                cmin += 1
             if c == '*':
                 cmax += 1
                 cmin = max(cmin - 1, 0)
+            if c == ')':
+                cmax -= 1
+                cmin = max(cmin - 1, 0)
             if cmax < 0:
-                return False
-        return cmin == 0
+                return False  # early stop the process when the upperbound < 0
+        return cmin == 0  # cmin == 0 means we can eliminate all the '('
 
 if __name__ == "__main__":
     sol = Solution()
