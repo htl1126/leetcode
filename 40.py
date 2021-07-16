a8102cd5af8fa0efe66589a415316185db732421
htl1126 <tlhuang@tlhuang.net>
2021-07-16 10:41:15 -0400

    Modified 40.py

--- 40.py
+++ 40.py
@@ -1,29 +1,24 @@
-class Solution(object):
-    def combinationSum(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        res, n = [], len(candidates)
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-
-        def dfs(begin, cur):
-            if sum(cur) == target:
-                res.append(cur)
+        ans, size = [], len(candidates)
+        def dfs(i, cur_path, cur_sum):
+            if cur_sum == target:
+                ans.append(cur_path)
             else:
-                i = begin
-                while i < n:
-                    if sum(cur + [candidates[i]]) <= target:
-                        dfs(i + 1, cur + [candidates[i]])
+                while i < size:
+                    if cur_sum + candidates[i] <= target: # early cut-off for saving time
+                        dfs(i + 1, cur_path + [candidates[i]], cur_sum + candidates[i])
                         i += 1
                         # avoid duplicates
-                        while i < n and candidates[i] == candidates[i - 1]:
+                        # for case [1, 1, 1, 2], target = 4, we just want to get one combination
+                        # for [1, 1, 1] where the sum is 2
+                        while i < size and candidates[i] == candidates[i - 1]:
                             i += 1
                     else:
                         return
-        dfs(0, [])
-        return res
+        dfs(0, [], 0)
+        return ans
 
 if __name__ == '__main__':
     sol = Solution()
