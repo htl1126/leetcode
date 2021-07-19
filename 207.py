75df91e9db2bd0dc8480fe3f2cc054e1c6188606
htl1126 <tlhuang@tlhuang.net>
2021-07-19 11:14:47 -0400

    Modified 207.py

--- 207.py
+++ 207.py
@@ -1,23 +1,18 @@
 import collections
 
 
-class Solution(object):
-    def canFinish(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: bool
-        """
-        suc, pre = collections.defaultdict(set), collections.defaultdict(set)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        suc, pre = collections.defaultdict(set), collections.defaultdict(int)
         for a, b in prerequisites:
             suc[b].add(a)
-            pre[a].add(b)
+            pre[a] += 1
         free = set(range(numCourses)) - set(pre)
         length = 0
         while free:
             n = free.pop()
             for b in suc[n]:
-                pre[b].discard(n)
+                pre[b] -= 1
                 if not pre[b]:
                     free.add(b)
             length += 1
