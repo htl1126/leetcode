762c5b0adb6b1fbc24623176188dd80722bfbdb0
htl1126 <tlhuang@tlhuang.net>
2021-07-19 14:23:32 -0400

    Modified 528.py

--- 528.py
+++ 528.py
@@ -2,30 +2,24 @@
 
 import random
 
-class Solution(object):
+class Solution:
 
-    def __init__(self, w):
-        """
-        :type w: List[int]
-        """
+    def __init__(self, w: List[int]):
         self.acc_arr = w[:]
-        for i in xrange(1, len(w)):
+        for i in range(1, len(w)):
             self.acc_arr[i] += self.acc_arr[i - 1]
         self.acc_sum = self.acc_arr[-1]
 
-    def pickIndex(self):
-        """
-        :rtype: int
-        """
+    def pickIndex(self) -> int:
         i = random.randint(1, self.acc_sum)
-        left, right = 0, len(self.acc_arr) - 1
-        while left != right:
-            mid = (left + right) / 2
-            if i > self.acc_arr[mid]:
-                left = mid + 1
+        l, r = 0, len(self.acc_arr) - 1
+        while l < r:
+            m = (l + r) // 2
+            if i > self.acc_arr[m]:
+                l = m + 1
             else:
-                right = mid
-        return left
+                r = m
+        return l
 
 
 # Your Solution object will be instantiated and called as such:
