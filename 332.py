7ba24320364ef9aa9ef6eccae1eb6b525e09e47f
htl1126 <tlhuang@tlhuang.net>
2021-07-23 17:09:54 -0400

    Modified 332.py

--- 332.py
+++ 332.py
@@ -2,20 +2,22 @@ import collections
 # ref: https://discuss.leetcode.com/topic/36370/short-ruby-python-java-c
 
 
-class Solution(object):
-    def findItinerary(self, tickets):
-        """
-        :type tickets: List[List[str]]
-        :rtype: List[str]
-        """
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
         targets = collections.defaultdict(list)
+        # after the loop, the locations of destinations are sorted reversely
         for a, b in sorted(tickets)[::-1]:
-            targets[a] += b,
+            targets[a].append(b)
         route = []
 
+        # this function traverses the graph nodes in a lexical order
+        # and constructs the route from the end
         def visit(airport):
             while targets[airport]:
-                visit(targets[airport].pop())
+                # visit locations in accending order and remove it from the map,
+                # so the same edge won't be taken for more than once
+                next_loc = targets[airport].pop()
+                visit(next_loc)
             route.append(airport)
         visit('JFK')
         return route[::-1]
