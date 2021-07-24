ca65e1f2332564a4c04f541b81b74c598aa44f95
htl1126 <tlhuang@tlhuang.net>
2021-07-24 19:22:31 -0400

    Modified 767.py

--- 767.py
+++ 767.py
@@ -1,15 +1,11 @@
 # Ref: https://leetcode.com/problems/reorganize-string/discuss/113435/4-lines-Python
 
-class Solution(object):
-    def reorganizeString(self, S):
-        """
-        :type S: str
-        :rtype: str
-        """
+class Solution:
+    def reorganizeString(self, S: str) -> str:
         a = sorted(sorted(S), key=S.count)  # Case "abba" needs double sorted()
-        h = len(a) / 2
+        h = len(a) // 2
         a[1::2], a[::2] = a[:h], a[h:]
-        return "".join(a) * (a[-1:] != a[-2:-1])
+        return "".join(a) * (a[-2] != a[-1])
 
 if __name__ == "__main__":
     sol = Solution()
