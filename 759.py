3ce391cd8daf5e98f58205cfcafc3b090d81fb1d
htl1126 <tlhuang@tlhuang.net>
2021-07-25 19:31:36 -0400

    Modified 759.py

--- 759.py
+++ 759.py
@@ -1,13 +1,13 @@
-# Ref: https://leetcode.com/problems/employee-free-time/discuss/170551/Simple-Python-9-liner-beats-97-(with-explanation)
+# Ref: https://leetcode.com/problems/employee-free-time/discuss/170551/Simple-Python-9-liner-beats-97-(with-explanation)
 
 class Solution:
-    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
-        intervals = sorted([i for employee in schedule for i in employee], key=lambda x: x.start)
-        ans, pre = [], intervals[0]
-        for i in intervals[1:]:
-            if i.start <= pre.end and i.end > pre.end:
-                pre.end = i.end
-            elif i.start > pre.end:
-                ans.append(Interval(pre.end, i.start))
-                pre = i
-        return ans
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        intervals = sorted([i for employee in schedule for i in employee], key=lambda x: x.start)
+        ans, pre = [], intervals[0]
+        for i in intervals[1:]:
+            if i.start <= pre.end and i.end > pre.end:
+                pre.end = i.end
+            elif i.start > pre.end:
+                ans.append(Interval(pre.end, i.start))
+                pre = i
+        return ans
