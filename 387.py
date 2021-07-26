890fb482b11f6b4c91fd8d8837bec082b4f1e990
htl1126 <tlhuang@tlhuang.net>
2021-07-26 16:51:11 -0400

    Modified 387.py

--- 387.py
+++ 387.py
@@ -1,22 +1,15 @@
 import collections
 
 
-class Solution(object):
-    def firstUniqChar(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        ctr = collections.OrderedDict()
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        d, ans = collections.defaultdict(list), len(s)
         for i, c in enumerate(s):
-            if c in ctr:
-                ctr[c] += i,
-            else:
-                ctr[c] = [i]
-        for c in ctr:
-            if len(ctr[c]) == 1:
-                return ctr[c][0]
-        return -1
+            d[c].append(i)
+        for c in d:
+            if len(d[c]) == 1 and d[c][0] < ans:
+                ans = d[c][0]
+        return ans if ans < len(s) else -1
 
 if __name__ == '__main__':
     sol = Solution()
