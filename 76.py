93e80f5a4882e473c27ccc33135b9431d68b1cbc
htl1126 <tlhuang@tlhuang.net>
2021-07-29 10:46:38 -0400

    Modified 76.py

--- 76.py
+++ 76.py
@@ -2,26 +2,22 @@
 import collections
 
 
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         # missing: the number of chars needed to for taking all the chars in t
         # need: freq of each char in the window; negative: we are having more than we need
         #                                        positive: we still need more
         need, missing = collections.Counter(t), len(t)
-        i = I = J = 0
+        i, I, J = 0, 0, 0
         for j, c in enumerate(s, 1):
-            missing -= need[c] > 0
+            if need[c] > 0:
+                missing -= 1
             need[c] -= 1
-            if not missing:  # we have collected all the chars in t
-                while i < j and need[s[i]] < 0:  # shrink the window
+            if missing == 0:  # we have collected all the chars in t
+                while i <= j and need[s[i]] < 0:  # shrink the window
                     need[s[i]] += 1
                     i += 1
-                if not J or j - i < J - I:  # I and J are 0's in the beginning
+                if J == 0 or j - i < J - I:  # I and J are 0's in the beginning
                     I, J = i, j
         return s[I:J]
 
