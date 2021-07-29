4331fea76e5d45f7b5eaa9dd56f1e4727e44d980
htl1126 <tlhuang@tlhuang.net>
2021-07-29 12:22:30 -0400

    Modified 158.py

--- 158.py
+++ 158.py
@@ -5,7 +5,7 @@
 
 class Solution:
     def __init__(self):
-        self.c_buf = []
+        self.c_buf = collections.deque()
     
     def read(self, buf: List[str], n: int) -> int:
         ans = 0
@@ -13,12 +13,16 @@ class Solution:
             buf4 = [""] * 4
             l = read4(buf4)
             if l:
-                self.c_buf += buf4[:l]
+                self.c_buf += buf4[:l]  # just read l elements
             read_len = min(len(self.c_buf), n)
             for _ in range(read_len):
-                buf[ans] = self.c_buf.pop(0)
+                # popleft() is of O(1) time complexity
+                buf[ans] = self.c_buf.popleft()  # we should know where to put data exactly
                 ans += 1
                 n -= 1
+            # if read_len == n, we can just leave the loop as n becomes zero.
+            # if read_len == len(self.c_buf), we can also leave since we cannot read
+            # more characters.
             if read_len < 4:
                 break
         return ans
