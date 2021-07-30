17f8c7132e5c875d86af710d0498d5255cc781cf
htl1126 <tlhuang@tlhuang.net>
2021-07-30 17:29:06 -0400

    Modified 875.py

--- 875.py
+++ 875.py
@@ -1,12 +1,12 @@
-# Ref: https://leetcode.com/problems/koko-eating-bananas/discuss/152324/JavaC%2B%2BPython-Binary-Search
+# Ref: https://leetcode.com/problems/koko-eating-bananas/discuss/152324/JavaC%2B%2BPython-Binary-Search
 
 class Solution:
-    def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        l, r = 1, max(piles)
-        while l < r:
-            m = (l + r ) // 2
-            if sum((p + m - 1) // m for p in piles) > h:
-                l = m + 1
-            else:
-                r = m
-        return l
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        while l < r:
+            m = (l + r) // 2
+            if sum((p + m - 1) // m for p in piles) > h:
+                l = m + 1
+            else:
+                r = m
+        return l
