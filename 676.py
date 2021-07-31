1b78727a1b5786775dfd0ffff9466ce298417e79
htl1126 <tlhuang@tlhuang.net>
2021-07-31 19:36:16 -0400

    Modified 676.py

--- 676.py
+++ 676.py
@@ -14,6 +14,12 @@ class MagicDictionary:
         self.near = collections.Counter(w[:i] + '*' + w[i + 1:] for w in self.words for i in range(len(w)))
 
     def search(self, searchWord: str) -> bool:
+        # case self.near[w] > 1:
+        #     self.near could be {"ab*": 2} from "aba" and "abc", so the searchWord could be "aba", "abc" or "abb"
+        #     to guarantee a valid transformation
+        # case self.near[w] == 1:
+        #     self.near could be {"ab*": 1} from "aba", so the searchWord should not be "aba" to guarantee a valid
+        #     transformation
         return any(self.near[w] > 1 or self.near[w] == 1 and searchWord not in self.words for w in [searchWord[:i] + '*' + searchWord[i + 1:] for i in range(len(searchWord))])
 
 
