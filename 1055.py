6dca33e1d3e7854ff9061e7a2f034ac36a03c3ee
htl1126 <tlhuang@tlhuang.net>
2021-08-02 12:24:13 -0400

    Modified 1055.py

--- 1055.py
+++ 1055.py
@@ -1,17 +1,15 @@
 # Ref: https://leetcode.com/problems/shortest-way-to-form-string/discuss/399353/Python-O(M-%2B-N)-using-hash-table.-Space-O(M).-With-notes.
 # Algo: greedy
 
-class Solution(object):
-    def shortestWay(self, source, target):
-        """
-        :type source: str
-        :type target: str
-        :rtype: int
-        """
-        after, indices = {}, {}
-        for i in reversed(xrange(-1, len(source))):
-            after[i] = dict(indices)  # make a copy of 'indices', not the ref address
-            indices[source[i]] = i  # keep the nearest index of each char
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        after, indexes = {}, {}
+        # indices: inverted index for each char in 'source'
+        # after: after[i] stores inverted indexes for chars after source[i]
+        #        after[-1] == indices
+        for i in range(len(source) - 1, -2, -1):
+            after[i] = dict(indexes)  # make a copy of 'indices', not the ref address
+            indexes[source[i]] = i  # keep the nearest index of each char
         i, ans = -1, 1
         for c in target:
             if c not in after[-1]:
