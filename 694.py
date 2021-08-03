bb4b7dd7f70c1962ecd9ecc6838da4b515d0ee88
htl1126 <tlhuang@tlhuang.net>
2021-08-03 09:26:35 -0400

    Modified 694.py

--- 694.py
+++ 694.py
@@ -1,25 +1,21 @@
 # Ref: https://leetcode.com/problems/number-of-distinct-islands/discuss/161867/Python-Very-Clean-and-Easy-to-Understand
 # Algo: DFS
 
-class Solution(object):
-    def numDistinctIslands(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
         def helper(i, j, direct):
             if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == 1:
-                grid[i][j] = 0
+                grid[i][j] = 0  # we don't want to visit the same island twice
                 self.steps += direct
                 helper(i - 1, j, 'u')
                 helper(i, j - 1, 'l')
                 helper(i, j + 1, 'r')
                 helper(i + 1, j, 'd')
-                self.steps += 'b'
+                self.steps += 'b'  # this line is important
         distinctIslands = set()
         self.steps = ''
-        for i in xrange(len(grid)):
-            for j in xrange(len(grid[0])):
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
                 if grid[i][j] == 1:
                     helper(i, j, 'o')
                     distinctIslands.add(self.steps)
