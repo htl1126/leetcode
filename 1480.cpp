1c19bc4d90d488359b0fdcf74cc1d3c34e2ee9dc
htl1126 <tlhuang@tlhuang.net>
2021-09-06 01:22:16 -0400

    Created 1480.cpp

---
+++ 1480.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        for (auto i = 1; i < nums.size(); i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
