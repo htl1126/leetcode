0acfb94d7b3a86dd37a37c9929d7c5904cac7da2
htl1126 <tlhuang@tlhuang.net>
2021-09-12 19:39:25 -0400

    Created 2000.py

---
+++ 2000.py
@@ -0,0 +1,6 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        i = word.index(ch)
+        return word[:i + 1][::-1] + word[i + 1:]
