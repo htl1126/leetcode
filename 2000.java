2b7facc69d48422483a4067977cc88ccbac70708
htl1126 <tlhuang@tlhuang.net>
2021-09-12 20:02:34 -0400

    Created 2000.java

---
+++ 2000.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int i = word.indexOf(ch);
+        if (i != -1) {
+            char s[] = word.toCharArray(), t;
+            int j = 0;
+            while (j < i) {
+                t = s[i];
+                s[i] = s[j];
+                s[j] = t;
+                j++;
+                i--;
+            }
+            return new String(s);
+        }
+        return word;
+    }
+}
