622d331716825c7b7c76721cb8a8b8a112a82882
htl1126 <tlhuang@tlhuang.net>
2021-10-17 12:27:12 +0800

    Created 2043.py

---
+++ 2043.py
@@ -0,0 +1,31 @@
+class Bank:
+
+    def __init__(self, balance: List[int]):
+        self.balance = balance
+        self.n = len(balance)
+
+    def transfer(self, account1: int, account2: int, money: int) -> bool:
+        if 1 <= account1 <= self.n and 1 <= account2 <= self.n and self.balance[account1 - 1] >= money:
+            self.balance[account1 - 1] -= money
+            self.balance[account2 - 1] += money
+            return True
+        return False
+
+    def deposit(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n:
+            self.balance[account - 1] += money
+            return True
+        return False
+
+    def withdraw(self, account: int, money: int) -> bool:
+        if 1 <= account <= self.n and self.balance[account - 1] >= money:
+            self.balance[account - 1] -= money
+            return True
+        return False
+
+
+# Your Bank object will be instantiated and called as such:
+# obj = Bank(balance)
+# param_1 = obj.transfer(account1,account2,money)
+# param_2 = obj.deposit(account,money)
+# param_3 = obj.withdraw(account,money)
