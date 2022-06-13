baa86ce3a22d2d785da3a70a25ab5ee937eaebc5
htl1126 <tlhuang@tlhuang.net>
2022-06-13 15:14:33 +0800

    Created 933.py

---
+++ 933.py
@@ -0,0 +1,17 @@
+# Ref: https://leetcode.com/problems/number-of-recent-calls/discuss/189239/JavaPython-3-Five-solutions%3A-TreeMap-TreeSet-ArrayList-Queue-Circular-List.
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = collections.deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+        return len(self.q)
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
