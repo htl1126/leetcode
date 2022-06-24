918f1e55fde9ba510c0a0b79937f0f4baf7cba77
htl1126 <tlhuang@tlhuang.net>
2022-06-24 19:49:42 +0800

    Created 450.py

---
+++ 450.py
@@ -0,0 +1,30 @@
+# Ref: https://leetcode.com/problems/delete-node-in-a-bst/discuss/821420/Python-O(h)-solution-explained
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key == root.val:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+            temp = root.right
+            while temp.left:
+                temp = temp.left
+            root.val = temp.val
+            root.right = self.deleteNode(root.right, root.val)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            root.right = self.deleteNode(root.right, key)
+
+        return root
