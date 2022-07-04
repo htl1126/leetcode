c5ab86c8c8e7614f61b6b2661b97b7492b82c7f1
htl1126 <tlhuang@tlhuang.net>
2022-07-04 14:24:24 +0800

    Created 1689.py

---
+++ 1689.py
@@ -0,0 +1,5 @@
+# Ref: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/discuss/970318/JavaC%2B%2BPython-Just-return-max-digit
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
