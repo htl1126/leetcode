1020a1972a394d9ab7911ab30b0585ced855911e
htl1126 <tlhuang@tlhuang.net>
2022-07-09 01:14:33 +0800

    Created 1171.py

---
+++ 1171.py
@@ -0,0 +1,24 @@
+# Ref: https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/discuss/366319/JavaC%2B%2BPython-Greedily-Skip-with-HashMap
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        seen = {}
+        prefix = 0
+        seen[0] = dummy = ListNode(0)
+        dummy.next = head
+        while head:
+            prefix += head.val
+            seen[prefix] = head
+            head = head.next
+        head = dummy
+        prefix = 0
+        while head:
+            prefix += head.val
+            head.next = seen[prefix].next
+            head = head.next
+        return dummy.next
