25ad806976d4b6e71806c36f45c768c9fea6e67e
htl1126 <tlhuang@tlhuang.net>
2022-09-24 09:23:16 +0800

    Created 1237.py

---
+++ 1237.py
@@ -0,0 +1,23 @@
+# Ref: https://leetcode.com/problems/find-positive-integer-solution-for-a-given-equation/discuss/414249/JavaC%2B%2BPython-O(X%2BY)
+
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+
+class Solution:
+    def findSolution(self, customfunction: 'CustomFunction', z: int) -> List[List[int]]:
+        ans = []
+        y = 1000
+        for x in range(1, 1001):
+            while y > 1 and customfunction.f(x, y) > z:
+                y -= 1
+            if customfunction.f(x, y) == z:
+                ans.append([x, y])
+        return ans
