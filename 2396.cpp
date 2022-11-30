798ccf98ed7d11ef22dbcc88537c9e94e15cbd2e
htl1126 <tlhuang@tlhuang.net>
2022-12-01 01:40:10 +0800

    Created 2396.cpp

---
+++ 2396.cpp
@@ -0,0 +1,8 @@
+// Ref: https://leetcode.com/problems/strictly-palindromic-number/discuss/2524702/return-false
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
